7061b5607cdc8a9483d5cd6c2eb989f8b369b71f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-09-25 21:24:07 +0200

    Update totalAmountsDisplay.js

--- totalAmountsDisplay.js
+++ totalAmountsDisplay.js
@@ -1,1 +1,14 @@
+setInterval(function totalAmountsDisplay(){
+        let DpAmount1 = localStorage.getItem('totalAmountDP');
+        let CcAmount1 = localStorage.getItem('totalAmountCC');
+        let OcAmount1 = localStorage.getItem('totalAmountOC');
+        let BwAmount1 = localStorage.getItem('totalAmountBW');
+        let PrAmount1 = localStorage.getItem('totalAmountPR');
+        let NlAmount1 = localStorage.getItem('totalAmountNL');
+        let CCesAmount1 = localStorage.getItem('totalAmountCCES');
+        let PeAmount1 = localStorage.getItem('totalAmountPE');
 
+
+
+}
+totalAmountsDisplay();
