f0c3eca5ee0aad796e32aeb1a58526ed285915cd
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-01 17:51:35 +0200

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -5,5 +5,12 @@ Require 'dbh.php';
 $username=$_POST['uid'];
 $email=$_POST['email'];
 $password=$_POST['pwd'];
-$username=$_POST['pwdrepeat'];
+$passwordrepeat=$_POST['pwdrepeat'];
+
+if(empty($username)||empty($email)||empty($password)||empty($passwordrepeat)||){
+   header("Location:signup.php?error=emptyfields&uid=".$username.".&email=".$email);
+     exit();
+}                  
+
+
 }
