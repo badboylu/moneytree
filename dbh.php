6b38b42fda8465645324bf9ed4e065ca6977eb43
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-01 17:28:06 +0200

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,1 +1,10 @@
+<?php
+$servername="";
+$dBUsername="";
+$dBPassword="";
+$dBName="";
 
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+
+if(!conn){  
+    die("connection failed:" mysqli_connect_error());
