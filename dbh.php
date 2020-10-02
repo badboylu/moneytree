a5b03053ad589cb60452528fba38ebaf4e2192a0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-02 09:38:42 +0200

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,7 +1,7 @@
 <?php
-$servername="";
-$dBUsername="";
-$dBPassword="";
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="d7228c67";
 $dBName="";
 
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
