44499fcc243d6a638ec968031b15650771af033f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-02 10:14:08 +0200

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -10,32 +10,47 @@ $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
 
 if(empty($username)||empty($email)||empty($password)||empty($passwordrepeat)){
-   header("Location:signup.php?error=emptyfields&uid=".$username.".&email=".$email);
+   header("Location:signup.html?error=emptyfields&uid=".$username.".&email=".$email);
      exit();
 }                  
 
 else if(!FILTER_VAR($email, FILTER_VALIDATE_EMAIL)&&!preg_match("/^[a-zA-Z0-9]*$/",$username)){        
-   header("Location:signup.php?error=invalidemail&uid);
+   header("Location:signup.html?error=invalidemail&uid);
      exit();
 }
 
 else if(!FILTER_VAR($email, FILTER_VALIDATE_EMAIL)){        
-   header("Location:signup.php?error=invalidemail&uid=".$username);
+   header("Location:signup.html?error=invalidemail&uid=".$username);
      exit();
 }
 
 else if(!preg_match("/^[a-zA-Z0-9]*$/",$username)){        
-   header("Location:signup.php?error=invaliduid&email=".$email);
+   header("Location:signup.html?error=invaliduid&email=".$email);
      exit();
 }
 
 else if($password !== $passwordrepeat){
-   header("Location:signup.php?error=passwordcheck&uid=".$username.".&email=".$email);
+   header("Location:signup.html?error=passwordcheck&uid=".$username.".&email=".$email);
      exit();
 }
 
+else{
+   $sql="SELECT uidUsers FROM users WHERE uidUsers=?";
+   $stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:signup.html?error=sqlerror");
+       exit();
+}
+  else{
+   mysqli_stmt_bind_param($stmt,"s",$username);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $resultCheck=mysqli_stmt_num_rows($stmt);
+  if($resultCheck > 0){
+       header("Location:signup.html?error=usernametaken&email=".$email");
+       exit();
+}
+}
 
-
-
-
+ }
 }
