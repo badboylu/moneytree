0df090d0393b69d01f3331c2457d841774574b47
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-03 16:34:20 +0200

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -2,7 +2,7 @@
 $servername="us-cdbr-east-02.cleardb.com";
 $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
-$dBName="users";
+$dBName="heroku_61db5a5cdc2dfd8";
 
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 
