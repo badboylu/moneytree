2e9033f56f980fcf99c67094c9cdaae383a799b6
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-04 17:19:10 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -12,9 +12,22 @@ if(isset($_POST['login'])){
    exit();
  }
   else{
+
    $sql="SELECT * FROM users WHERE uidUsers=?";
    $stmt=mysqli_stmt_init($conn) 
 
-
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+     header("Location:signing.html?error=sqlerror");
+     exit();
+  }
+   else{
+     mysqli_stmt_bind_params($stmt,"ss",$mailuid,$mailuid);
+     mysqli_stmt_execute($stmt);
+     $result=mysqli_stmt_get_result($stmt);
+      
+      if($row=mysqli_fetch_assoc($result)){
+       
+   }
+  }
  }
 }
