cc92ca970d3a6543c30d9d9cab6be6957d70b519
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-07 17:31:37 +0200

    Update resetPassword.php

--- resetPassword.php
+++ resetPassword.php
@@ -7,4 +7,14 @@ if(isset($_POST['reset-submit'])){
  $password=$_POST["pwd"];
  $passwordRepeat=$_POST["pwdRepeat"];
 
+  if(empty($password) || empty($passwordRepeat)){
+   header("Location:createpwd.html?error=empty fields");
+   exit();
+  }
+
+  else if($password!=$passwordRepeat){
+   header("Location:createpwd.html?error=empty fields");
+   exit();
+  }
+
 }
