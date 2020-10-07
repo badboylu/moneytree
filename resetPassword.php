1efb4ad88d2a7f3a86ee2d103196220f387e6b73
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-07 17:24:39 +0200

    Create resetPassword.php

---
+++ resetPassword.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_POST['reset-submit'])){
+
+ $selector=$_POST["selector"];
+ $validator=$_POST["validator"];
+ $password=$_POST["pwd"];
+ $passwordRepeat=$_POST["pwdRepeat"];
+
+}
