fa9fa767c2982645fbfaa1840a168e7182a86c90
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-08 08:51:25 +0200

    Update resetPassword.php

--- resetPassword.php
+++ resetPassword.php
@@ -50,7 +50,16 @@ if(isset($_POST['reset-submit'])){
            exit();
           }
           else{
-           
+           mysqli_stmt_bind_param($stmt,"s",$tokenEmail);
+           mysqli_stmt_execute($stmt);
+           $result=mysqli_stmt_get_result($stmt);
+            if(!$row=mysqli_fetch_assoc($result)){
+             echo "Please re-submit request";
+             exit();
+            }
+            else{
+
+            }
           }
         }
       }
