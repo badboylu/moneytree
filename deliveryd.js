dbc822174d408625d3d38daf9101e739fa68d1cf
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-13 23:28:56 +0200

    Update deliveryd.js

--- deliveryd.js
+++ deliveryd.js
@@ -2,7 +2,7 @@ function deliveryd(){
 
    if(document.moneytree.City.value=='Tshwane/Pretoria'){
      document.getElementById("townsPTA").style.display = "block";
-     document.getElementById("DPgrams").style.display = "block";
+     document.getElementById("townsJHB").style.display = "block";
    }
  }
 
