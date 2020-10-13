f0e614558f7c49e1ea7aff7cc439a3df8dc1ef62
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-13 23:51:58 +0200

    Update deliveryd.js

--- deliveryd.js
+++ deliveryd.js
@@ -1,10 +1,10 @@
 function deliveryd(){
 
-   if(document.moneytree.City.value=='Tshwane/Pretoria'){
+   if(document.frmmoneytree.City.value=='Tshwane/Pretoria'){
      document.getElementById("townsPTA").style.display = "block";
      document.getElementById("townsJHB").style.display = "none";
    }
-   if(document.moneytree.City.value=='Johannesburg'){
+   if(document.frmmoneytree.City.value=='Johannesburg'){
      document.getElementById("townsPTA").style.display = "none";
      document.getElementById("townsJHB").style.display = "block";
    }
