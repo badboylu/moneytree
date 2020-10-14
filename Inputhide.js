773c5c892a112f4e64fa0d4460cfd0a000fbcdcb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-14 19:58:08 +0200

    Update Inputhide.js

--- Inputhide.js
+++ Inputhide.js
@@ -7,4 +7,23 @@ setInterval (function hideJHB(){
 	}
 }, 1000);
 
+setInterval (function hidePTA(){
+   var City = document.getElementById('city').value;
+        if (City=='Johannesburg'){
+		document.getElementById("hideJHBtown").style.display = "block";
+	}else{
+		document.getElementById("hideJHBtown").style.display = "none";
+	}
+}, 1000);
+
+setInterval (function showPTAeast(){
+   var City = document.getElementById('city').value;
+        if (City=='Johannesburg'){
+		document.getElementById("hideJHBtown").style.display = "block";
+	}else{
+		document.getElementById("hideJHBtown").style.display = "none";
+	}
+}, 1000);
+
 hideJHB();
+hidePTA();
