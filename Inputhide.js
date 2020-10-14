ae34eeecb8885c0cd6c5b3586b231d9ff354150e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-14 19:40:03 +0200

    Create Inputhide.js

---
+++ Inputhide.js
@@ -0,0 +1,9 @@
+setInterval (function hideJHB(){
+        if (){
+		document.getElementById("BW").style.display = "none";
+	}else{
+		document.getElementById("BW").style.display = "block";
+	}
+}, 1000);
+
+hideJHB();
