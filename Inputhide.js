427c56f552ce65ad9ca669dc23e5720dac393ec0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-16 23:44:10 +0200

    Update Inputhide.js

--- Inputhide.js
+++ Inputhide.js
@@ -27,9 +27,8 @@ setInterval (function showPTAeast(){
 }, 1000);
 
 setInterval (function showStructuretype(){
-   var City = document.getElementById('city').value;
    var Town = document.getElementById('townsPTA').value;
-        if (Town && City){
+        if (Town){
 		document.getElementById("hidecribtype").style.display = "block";
 	}
 }, 1000);
@@ -43,8 +42,48 @@ setInterval (function showEstate(){
 	}
 }, 1000);
 
+setInterval (function showStreet(){
+   var Estate = document.getElementById('Cribtype').value;
+        if (Estate=='Estate'){
+		document.getElementById("hideStreet").style.display = "block";
+	}else{
+		document.getElementById("hideStreet").style.display = "none";
+	}
+}, 1000);
+
+setInterval (function showNumber(){
+   var Estate = document.getElementById('Cribtype').value;
+        if (Estate=='Estate'){
+		document.getElementById("hideNumber").style.display = "block";
+	}else{
+		document.getElementById("hideStreet").style.display = "none";
+	}
+}, 1000);
+
+setInterval (function showContactNumber(){
+   var Estate = document.getElementById('Cribtype').value;
+        if (Estate=='Estate'){
+		document.getElementById("hideContactNumber").style.display = "block";
+	}else{
+		document.getElementById("hideContactNumber").style.display = "none";
+	}
+}, 1000);
+
+setInterval (function showContactName(){
+   var Estate = document.getElementById('Cribtype').value;
+        if (Estate=='Estate'){
+		document.getElementById("hideName").style.display = "block";
+	}else{
+		document.getElementById("hideName").style.display = "none";
+	}
+}, 1000);
+
 hideJHB();
 hidePTA();
 showPTAeast();
 showStructuretype();
 showEstate();
+showStreet();
+showNumber();
+showContactNumber();
+showContactName();
