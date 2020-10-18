60300aa961baa3244f9c4aeec8ce84de0a9c27bf
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-18 22:28:03 +0200

    Create Form.js

---
+++ Form.js
@@ -0,0 +1,13 @@
+const Fill = document.querySelectorAll('.Blanks');
+
+Fill.forEach(Blanks =>{
+        Blanks.addEventListener('click', () => {
+                Fillblanks();              
+
+})
+}
+)
+
+function Fillblanks(){
+
+}
