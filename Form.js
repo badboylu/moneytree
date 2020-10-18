e13baee818c6770b24a0291a9dfb188fda641982
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-10-18 23:01:08 +0200

    Update Form.js

--- Form.js
+++ Form.js
@@ -9,5 +9,11 @@ Fill.forEach(Blanks =>{
 )
 
 function Fillblanks(){
-
+   var Mall = document.getElementById('city').value;
+   var Estate = document.getElementById('city').value;
+   var Company = document.getElementById('city').value;
+   var Shop = document.getElementById('city').value;
+   var Complex = document.getElementById('city').value;
+   var Building = document.getElementById('city').value;
+   var Street = document.getElementById('city').value;     
 }
