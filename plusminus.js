b5f2372a41853577062da71f4eea193efc69dacd
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-04 19:26:29 +0200

    Update plusminus.js

--- plusminus.js
+++ plusminus.js
@@ -1,7 +1,8 @@
-var iconsBW = document.getElementById('iconsAddBW');
-
-iconsBW.style.cursor = 'pointer';
-iconsBW.onclick = function() {
+var iconsAddBW = document.getElementById('iconsAddBW');
+var iconsMinusBW = document.getElementById('iconsAddBW');
+iconsAddBW.style.cursor = 'pointer';
+iconsMinusBW.style.cursor = 'pointer';
+iconsAddBW.onclick = function() {
   
   var priceBW = 45;
    
@@ -25,4 +26,33 @@ iconsBW.onclick = function() {
                 localStorage.setItem('PacksBW', 1 );       
                 document.querySelector('.gramsamountBW').textContent = parseInt(gramsNumberaddBW);
                }
+};
+
+iconsMinusBW.onclick = function() {
+   var priceBW = 45;
+    localStorage.setItem('totalCostBW', priceBW);
+        var cartCostBW= localStorage.getItem('totalCostBW', priceBW);
+        cartCostBW = parseInt(cartCostBW);
+        if(cartCostBW) {
+                localStorage.setItem('totalCostBW', cartCostBW);
+        }else{
+                localStorage.setItem('totalCostBW', parseInt(cartCostBW));
+        }
+   let gramsNumbersminusBW = localStorage.getItem('PacksBW'); 
+        gramsNumbersminusBW = parseInt(gramsNumbersminusBW);
+               if(gramsNumbersminusBW) {
+                localStorage.setItem('PacksBW', gramsNumbersminusBW = (gramsNumbersminusBW - 1));
+                document.querySelector('.gramsamountBW').textContent = parseInt(gramsNumbersminusBW) -1;
+               }else{
+                document.querySelector('.gramsamountBW').textContent = 0;   
+}
+   let checkgramsBW = localStorage.getItem("PacksBW");
+        checkgramsBW = parseInt(checkgramsBW);
+        if(checkgramsBW < 1){
+                localStorage.removeItem("PacksBW");
+                localStorage.removeItem("totalAmountBW");
+                localStorage.removeItem("totalCostBW");
+                localStorage.removeItem("cartNumberBW");
+        }
+
 };
