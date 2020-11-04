22ce3ac0aedcb5785a27ea6001f0c7a485a42aac
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-04 18:55:01 +0200

    Create plusminus.js

---
+++ plusminus.js
@@ -0,0 +1,28 @@
+var iconsBW = document.getElementById('');
+
+iconsBW.style.cursor = 'pointer';
+iconsBW.onclick = function() {
+  
+  var priceBW = 45;
+   
+   localStorage.setItem('totalCostBW', priceBW);
+   var cartCostBW= localStorage.getItem('totalCostBW', priceBW);
+   cartCostBW = parseInt(cartCostBW);
+        if(cartCostBW) {
+                localStorage.setItem('totalCostBW', cartCostBW);
+        }else{
+                localStorage.setItem('totalCostBW', parseInt(cartCostBW));
+        }
+        
+   let ifavailableBW = localStorage.getItem('PacksBW')
+        ifavailableBW = parseInt(ifavailableBW);
+               if(ifavailableBW) {
+                localStorage.setItem('PacksBW',(ifavailableBW) + 1);
+                let gramsNumberaddBW = localStorage.getItem('PacksBW'); 
+                gramsNumberaddBW = parseInt(gramsNumberaddBW);
+                document.querySelector('.gramsamountBW').textContent =parseInt(gramsNumberaddBW);
+               }else{
+                localStorage.setItem('PacksBW', 1 );       
+                document.querySelector('.gramsamountBW').textContent = parseInt(gramsNumberaddBW);
+               }
+};
