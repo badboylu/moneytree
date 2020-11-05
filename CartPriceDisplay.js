c1c91d175b81e5386417c2060e260240a7591d8f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-05 23:46:39 +0200

    Update CartPriceDisplay.js

--- CartPriceDisplay.js
+++ CartPriceDisplay.js
@@ -1,7 +1,7 @@
-function cartPriceDisplay(){
+setInterval (function cartPriceDisplay(){
 let cartCost = localStorage.getItem('totalAmountNumber');
 cartCost = parseInt(cartCost);
 document.querySelector('.totalOrder').textContent = cartCost;
 document.querySelector('.totalOrder1').textContent = cartCost + 40;
-}
+},1000);
 cartPriceDisplay();
