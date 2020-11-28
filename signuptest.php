68651a88a84b73f683d1220f92f2a38735947d60
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 15:43:30 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -7,20 +7,12 @@ $dBName="heroku_61db5a5cdc2dfd8";
 
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 
-$username=$_POST['uid'];
-$email=$_POST['email'];
-$password=$_POST['pwd'];
-
-$sql="SELECT uidUsers FROM users WHERE uidUsers=?";
-$stmt=mysqli_stmt_init($conn);
-mysqli_stmt_bind_param($stmt,"s",$username);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_store_result($stmt);
-$sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
-$stmt=mysqli_stmt_init($conn);
-$hashedPwd=password_hash($password,PASSWORD_DEFAULT);
-mysqli_stmt_bind_param($stmt,"ss",$email,$password);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_store_result($stmt);
+if(!conn){  
+    header("Location:Signin.html?signup=unsuccessful");
+       exit();
+}else {        
+   header("Location:index.html?signup=successful");
+     exit();
+}
 
 ?>
