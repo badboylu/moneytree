8b26f69349d436f729227a214c74c9608c4bf3d7
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 17:21:59 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -19,24 +19,30 @@ if(empty($mailuid)||empty($password)){
 
     $stmt=mysqli_stmt_init($conn); 
  
-      mysqli_stmt_bind_params($stmt,"ss",$mailuid,$password);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
-
-if($row = mysqli_fetch_assoc($result)){
-        $pwdcheck=password_verify($password, $row['pwdUsers']);
-        exit();
-    }
-       else if($pwdcheck == true){
-        SESSION_START();
-        $_SESSION['userId']=$row['idUsers'];
-        $_SESSION['userUid']=$row['uidUsers'];
-         header("Location:signing.html?login=successful");
-         exit();
-    }
-       else{
-         header("Location:signing.html?error=wrongpassword");
-         exit();
-    
+     if(!mysqli_stmt_prepare($stmt, $sql)){
+      header("Location:signing.html?error=sqlerror");
+      exit();
+   }
+   else{
+    mysqli_stmt_bind_params($stmt,"ss",$mailuid,$mailuid);
+    mysqli_stmt_execute($stmt);
+    $result=mysqli_stmt_get_result($stmt);
+     
+     if($row=mysqli_fetch_assoc($result)){
+      $pwdcheck=password_verify($password, $row['pwdUsers']);
+      exit();
+  }
+     else if($pwdcheck == true){
+      SESSION_START();
+      $_SESSION['userId']=$row['idUsers'];
+      $_SESSION['userUid']=$row['uidUsers'];
+       header("Location:signing.html?login=successful");
+       exit();
+  }
+     else{
+       header("Location:signing.html?error=wrongpassword");
+       exit();
+  }
+ }
 
 ?>
