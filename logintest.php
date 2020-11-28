2a470b44e8e7ef1ad84034023bc2df7c7ab5dd9e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 16:53:58 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['logintest.php'])){
+if(isset($_POST['submit'])){
 $servername="us-cdbr-east-02.cleardb.com";
 $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
@@ -8,7 +8,8 @@ $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 
 if(!conn){  
-    die("connection failed:" mysqli_connect_error());
+    header("Location:index.html?login=unsuccessful");
+    exit(); 
 }else{
     header("Location:index.html?login=successful");
     exit(); 
