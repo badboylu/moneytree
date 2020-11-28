44f9d9e9fb7de2993677d1e3d614bb81ed015949
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 15:53:17 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -15,4 +15,40 @@ if(!conn){
      exit();
 }
 
+$sql="SELECT uidUsers FROM users WHERE uidUsers=?";
+   
+$stmt=mysqli_stmt_init($conn);
+
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Signin.html?error=sqlerror");
+       exit();
+}
+  else{
+   mysqli_stmt_bind_param($stmt,"s",$username);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $resultCheck=mysqli_stmt_num_rows($stmt);
+  if($resultCheck > 0){
+       header("Location:Signin.html?error=usernametaken&email=".$email);
+       exit();
+}
+  else{ 
+    $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+     header("Location:Signin.html?error=usernametaken&email=".$email);
+       exit();
+}
+    else{
+     $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
+     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$password);
+     mysqli_stmt_execute($stmt);
+     mysqli_stmt_store_result($stmt);
+     header("Location:index.html?signup=successful");
+       exit();
+            }
+        }
+    }
+
 ?>
+
