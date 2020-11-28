975ad6b39d477053a61671a2d95ce8ac390ebf30
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 17:13:21 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -22,28 +22,10 @@ if(empty($mailuid)||empty($password)){
      if(!mysqli_stmt_prepare($stmt, $sql)){
       header("Location:signing.html?error=sqlerror");
       exit();
+   }else{
+    header("Location:signing.html?error=sqlsuccess");
+    exit();
    }
-    else{
-      mysqli_stmt_bind_params($stmt,"ss",$mailuid,$mailuid);
-      mysqli_stmt_execute($stmt);
-      $result=mysqli_stmt_get_result($stmt);
-       
-       if($row=mysqli_fetch_assoc($result)){
-        $pwdcheck=password_verify($password, $row['pwdUsers']);
-        exit();
-    }
-       else if($pwdcheck == true){
-        SESSION_START();
-        $_SESSION['userId']=$row['idUsers'];
-        $_SESSION['userUid']=$row['uidUsers'];
-         header("Location:signing.html?login=verysuccessful");
-         exit();
-    }
-       else{
-         header("Location:signing.html?error=wrongpassword");
-         exit();
-    }
-   }
-  
+    
 
 ?>
