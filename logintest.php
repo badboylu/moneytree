56f10570fe18b35ff878a762a704b9fef1178ba0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-11-28 17:02:17 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -7,12 +7,43 @@ $dBName="heroku_61db5a5cdc2dfd8";
 
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 
-if(!conn){  
-    header("Location:index.html?login=unsuccessful");
-    exit(); 
-}else{
-    header("Location:index.html?login=successful");
-    exit(); 
-}
+$mailuid=$_POST['mailuid'];
+$password=$_POST['pwd'];
+
+if(empty($mailuid)||empty($password)){
+    header("Location:signing.html?error=emptyfields");
+    exit();
+  }
+ 
+    $sql="SELECT * FROM users WHERE uidUsers=?";
+
+    $stmt=mysqli_stmt_init($conn); 
+ 
+     if(!mysqli_stmt_prepare($stmt, $sql)){
+      header("Location:signing.html?error=sqlerror");
+      exit();
+   }
+    else{
+      mysqli_stmt_bind_params($stmt,"ss",$mailuid,$mailuid);
+      mysqli_stmt_execute($stmt);
+      $result=mysqli_stmt_get_result($stmt);
+       
+       if($row=mysqli_fetch_assoc($result)){
+        $pwdcheck=password_verify($password, $row['pwdUsers']);
+        exit();
+    }
+       else if($pwdcheck == true){
+        SESSION_START();
+        $_SESSION['userId']=$row['idUsers'];
+        $_SESSION['userUid']=$row['uidUsers'];
+         header("Location:signing.html?login=successful");
+         exit();
+    }
+       else{
+         header("Location:signing.html?error=wrongpassword");
+         exit();
+    }
+   }
+  
 
 ?>
