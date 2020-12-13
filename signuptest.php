d3ae4236c08afece28bc9b095d8b2d9b7f1d1997
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-14 00:08:14 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -9,22 +9,38 @@ $username=$_POST['uid'];
 $email=$_POST['email'];
 $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
-$sql1="SELECT uidUsers FROM users WHERE uidUsers=?";
+$hashedPwd=password_hash($password,PASSWORD_DEFAULT);
+$hashedEml=password_hash($email,PASSWORD_DEFAULT);
+$sql1="SELECT emailUsers FROM users WHERE emailUsers=?";
 $stmt1=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt1,$sql1)){
        header("Location:register.html?error=SQL1");
+       exit();
+}
+$sql3="SELECT pwdUsers FROM users WHERE pwdUsers=?";
+$stmt3=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt3,$sql3)){
+       header("Location:register.html?error=SQL3");
        exit();
 }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
        header("Location:register.html?error=invalidemail");
        exit();
 }
-   mysqli_stmt_bind_param($stmt1,"s",$email);
+   mysqli_stmt_bind_param($stmt1,"s",$hashedEml);
    mysqli_stmt_execute($stmt1);
    mysqli_stmt_store_result($stmt1);
-   $resultCheck=mysqli_stmt_num_rows($stmt1);
-   if($resultCheck > 0){
+   $resultCheck1=mysqli_stmt_num_rows($stmt1);
+   if($resultCheck1 > 0){
        header("Location:register.html?error=emailtaken=".$username);
+       exit();
+} 
+   mysqli_stmt_bind_param($stmt3,"s",$hashedPwd);
+   mysqli_stmt_execute($stmt3);
+   mysqli_stmt_store_result($stmt3);
+   $resultCheck3=mysqli_stmt_num_rows($stmt3);
+   if($resultCheck3 > 0){
+       header("Location:register.html?error=passwordtaken=".$username.$email);
        exit();
 } 
     $sql2="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
