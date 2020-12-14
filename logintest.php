a1a4626b53859df0f62fd52a18b0457fdf46f468
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-14 12:26:06 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -24,15 +24,8 @@ if (!$count>0){
 if (!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:Signin.html?error=SQL");
        exit();
-}
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_store_result($stmt);
-if (password_verify($password, $stmt)) {
+}  
     header("Location:index.html?login=successful");
     exit();
-}else {
-    header("Location:Signin.html?error=wrngpwd");
-    exit();
-}
 }   
 ?>
