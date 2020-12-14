3bc94bad75c3bb73ebff3b2142c3f9f1ce734ccc
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-14 11:27:32 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -9,34 +9,34 @@ $username=$_POST['uid'];
 $email=$_POST['email'];
 $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
-$sql1="SELECT emailUsers FROM users WHERE emailUsers=?";
-$stmt1=mysqli_stmt_init($conn);
-  if(!mysqli_stmt_prepare($stmt1,$sql1)){
-       header("Location:register.html?error=SQL1");
+$sql="SELECT emailUsers FROM users WHERE emailUsers=?";
+$stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.html?error=SQL");
        exit();
 }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
        header("Location:register.html?error=invalidemail");
        exit();
 }
-   mysqli_stmt_bind_param($stmt3,"s",$email);
-   mysqli_stmt_execute($stmt3);
-   mysqli_stmt_store_result($stmt3);
-   $resultCheck3=mysqli_stmt_num_rows($stmt3);
-   if($resultCheck3 > 0){
+   mysqli_stmt_bind_param($stmt,"s",$email);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $resultCheck=mysqli_stmt_num_rows($stmt);
+   if($resultCheck > 0){
        header("Location:register.html?error=emailexists=".$username);
        exit();
 } 
-    $sql2="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
-    $stmt2=mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt2,$sql2)){
-       header("Location:register.html?error=SQL2");
+    $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.html?error=SQL");
        exit();
 }
      $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
-     mysqli_stmt_bind_param($stmt2,"sss",$username,$email,$hashedPwd);
-     mysqli_stmt_execute($stmt2);
-     mysqli_stmt_store_result($stmt2);
+     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);
+     mysqli_stmt_execute($stmt);
+     mysqli_stmt_store_result($stmt);
      header("Location:index.html?signup=successful");
        exit();
       
