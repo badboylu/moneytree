6e8fcf6a36eff03a8ef3d16499638b3489a8cfea
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-14 12:43:41 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -4,17 +4,19 @@ $servername="us-cdbr-east-02.cleardb.com";
 $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
-
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-
 $uid=$_POST['uid'];
 $password=$_POST['pwd'];
-
-    
-    $sql="SELECT * FROM users WHERE emailUsers='$uid' OR uidUsers='$uid' ";
-    $query=mysqli_query($conn,$sql);
-    $count=mysqli_num_row($query):
-
+$sql="SELECT * FROM users WHERE emailUsers='?' OR uidUsers='?' ";
+$stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.html?error=SQL");
+       exit();
+}
+mysqli_stmt_bind_param($stmt,"ss",$uid,$uid);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $count=mysqli_stmt_num_rows($stmt);
 if (!$count>0){
     header("Location:Signin.html?error=wrnguid");
     exit();
