c7645538c18513998a626d8b8bba3f3e709bfa97
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-15 17:38:09 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -9,14 +9,30 @@ $username=$_POST['uid'];
 $email=$_POST['email'];
 $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
+
+$sql="SELECT uidUsers FROM users WHERE uidUsers=?";
+$stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.html?error=SQL1");
+       exit();
+}
+mysqli_stmt_bind_param($stmt,"s",$username);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $resultCheck=mysqli_stmt_num_rows($stmt);
+   if($resultCheck > 0){
+       header("Location:register.html?error=usernameexists);
+       exit();
+} 
+
 $sql="SELECT emailUsers FROM users WHERE emailUsers=?";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.html?error=SQL");
+       header("Location:register.html?error=SQL2");
        exit();
 }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       header("Location:register.html?error=invalidemail");
+       header("Location:register.html?error=invalidemail"."&username=".$username);
        exit();
 }
    mysqli_stmt_bind_param($stmt,"s",$email);
@@ -24,13 +40,13 @@ $stmt=mysqli_stmt_init($conn);
    mysqli_stmt_store_result($stmt);
    $resultCheck=mysqli_stmt_num_rows($stmt);
    if($resultCheck > 0){
-       header("Location:register.html?error=emailexists=".$username);
+       header("Location:register.html?error=emailexists"."&username=".$username);
        exit();
 } 
     $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
     $stmt=mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.html?error=SQL");
+       header("Location:register.html?error=SQL3");
        exit();
 }
      $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
