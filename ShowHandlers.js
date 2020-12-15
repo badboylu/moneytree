ec439fcb3b8b0669a4aec139ab0bc9c0fec0349f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-15 15:05:36 +0200

    Update ShowHandlers.js

--- ShowHandlers.js
+++ ShowHandlers.js
@@ -3,7 +3,7 @@ var errorh  = params.get('error');
 
 function show(){
   if(errorh =='wrnguid'){
-  document.getElementById("myText").value = params.get('username');
+  document.getElementById("uid").value = params.get('username');
   document.getElementById("username").style.display = "block";
  }else if(errorh=='wrngpwd'){
    document.getElementById("password").style.display = "block";
