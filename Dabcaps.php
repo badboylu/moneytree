1b147996a9d79a411e68c7fc4493459d153f66a3
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-18 02:59:03 +0200

    Update Dabcaps.php

--- Dabcaps.php
+++ Dabcaps.php
@@ -81,11 +81,11 @@ href="cannacaps.html" />
             <div class="menu-top-bar-container">
             <ul id="menu-top-bar" class="menu">
             <li id="menu-item-20484" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20484 menu-item-design-default item-event-hover">
-            <a href="cartpage.html">
+            <a href="Cart.php?username=<?php echo $_GET['username']; ?>">
             Cart
             </a></li>
             <li id="menu-item-20488" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20488 menu-item-design-default item-event-hover">
-            <a href="index.html">
+            <a href="Shop.php?username=<?php echo $_GET['username']; ?>"> 
             Shop
             </a></li></ul></div></div></div></div></div></div>
 	
@@ -96,7 +96,7 @@ href="cannacaps.html" />
 	<div class="menu-main-navigation-container">
 	<ul id="menu-main-navigation" class="menu">
 	<li id="menu-item-19422" class="dropdown-scroll menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-19422 menu-item-design-sized item-event-hover menu-item-has-children">
-	<a href="index.html">
+	<a href="Shop.php?username=<?php echo $_GET['username']; ?>"> 
 	Shop
 	</a>
 	<div class="sub-menu-dropdown color-scheme-dark">
@@ -206,13 +206,13 @@ href="cannacaps.html" />
 </div>
 <div class="site-logo">
 <div class="basel-logo-wrap"> 
-<a href="index.html" class="basel-logo basel-main-logo" rel="home"> 
+<a href="Shop.php?username=<?php echo $_GET['username']; ?>" class="basel-logo basel-main-logo" rel="home"> 
 <img  src="https://res.cloudinary.com/badboylu/image/upload/c_scale,h_350/v1595757623/website_logo_head_aswjjx.jpg" alt="Basel" /> 
 </a></div></div>
 <div class="right-column">
 <div class="search-button basel-search-full-screen"> <a href="#"> <i class="fa fa-search"></i> </a><div class="basel-search-wrapper"><div class="basel-search-inner"> <span class="basel-close-search">close</span><form role="search" method="get" id="searchform" class="searchform  basel-ajax-search" action="https://demo.xtemos.com/basel/"  data-thumbnail="1" data-price="1" data-count="5" data-post_type="product" data-symbols_count="3" data-sku="0"><div> <label class="screen-reader-text">Search for:</label> <input type="text" class="search-field" placeholder="Search for products" value="" name="s" id="s" /> <input type="hidden" name="post_type" id="post_type" value="product"> <button type="submit" id="searchsubmit" value="Search">Search</button></div></form><div class="search-results-wrapper"><div class="basel-scroll"><div class="basel-search-results basel-scroll-content"></div></div></div></div></div></div>
 <div> 
-	<a href="cartpage.html"> 
+	<a href="Cart.php?username=<?php echo $_GET['username']; ?>"> 
 	<span class="basel-cart-totals"> 
 		<span class="basel-cart-numberCart">
 		0
@@ -238,7 +238,8 @@ href="cannacaps.html" />
 </h1>
 Edibles range
 </h1>
-<div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#"><a href="index.html" rel="v:url" property="v:title">
+<div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+<a href="Shop.php?username=<?php echo $_GET['username']; ?>" rel="v:url" property="v:title">
 Shop
 </a> 
 <span class="current">
@@ -271,7 +272,7 @@ Cannacaps
 <span class="title-after_title">
 Batch is 8 capsules of 50mg each
 </span></div><div class="woocommerce"><div class="single-product" data-product-page-preselected-id="0"><div class="single-breadcrumbs-wrapper"><div class="container"> <a href="javascript:baselThemeModule.backHistory()" class="basel-back-btn basel-tooltip"><span>Back</span></a><nav class="woocommerce-breadcrumb">
-<a href="https://demo.xtemos.com/basel">
+<a href="Shop.php?username=<?php echo $_GET['username']; ?>"> 
 Shop
 </a><span class="breadcrumb-last"> 
 Cannacaps
@@ -421,7 +422,7 @@ Edibles
 </div>
 <div class="basel-toolbar icons-design-line basel-toolbar-label-show">
 <div class="basel-toolbar-shop basel-toolbar-item"> 
-<a href="index.html"> 
+<a href="Shop.php?username=<?php echo $_GET['username']; ?>"> 
 <span class="basel-toolbar-label"> 
 Shop 
 </span> </a></div>
@@ -432,7 +433,7 @@ Shop
 <img src="https://img.icons8.com/pastel-glyph/30/000000/like--v1.png"/>
 </div>
 <div> 
-<a href="cartpage.html"> 
+<a href="Cart.php?username=<?php echo $_GET['username']; ?>"> 
 <img src="https://img.icons8.com/android/21/000000/shopping-cart.png"/>
 <span class="basel-toolbar-label">
 Cart 
