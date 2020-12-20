a40d0e725dd1fb8e9dbbd74f68f7ea52eb08879d
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 00:46:53 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -33,7 +33,8 @@ if (!$count>0){
   
    while (mysqli_stmt_fetch($stmt)) { 
     if (password_verify($password, $hash)) {
-    $token=random_bytes(32);
+    $random=random_bytes(32);
+    $token=var_dump(bin2hex($random));
     mysqli_stmt_close($stmt);
     $sql="INSERT INTO userauth2 (username,token) VALUES (?,?);";
     $stmt= mysqli_stmt_init($conn);
