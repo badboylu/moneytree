81d25103c4cc69a049f85fc705971f02a6f6c22a
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 12:42:19 +0200

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -5,13 +5,15 @@
    $dBPassword="1e8f896b7da9e41";
    $dBName="heroku_61db5a5cdc2dfd8";
    $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-   $sql="DELETE FROM userauth2 WHERE token='$_GET['auth']'";
-   mysqli_query("DELETE FROM userauth2 where token='$_GET['auth']'");
-  if(mysqli_query($conn,$sql)){
-       header("Location:register.html?error=SQL1");
-       exit();
-}
+   mysqli_query("DELETE FROM userauth2 WHERE token='$_GET['auth']'");
+   $check=mysqli_query("SELECT * FROM userauth2 WHERE token='$_GET['auth']'");
+ if(!mysqli_num_rows($check)>0)}
    header("Location:Signin.php?logout=successful");
    exit();
+}
+ else{
+   header("Location:Signin.php?logout=notsuccessful");
+   exit();
+}
 
 ?>
