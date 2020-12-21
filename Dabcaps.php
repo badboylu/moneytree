28b606e28bbc90668d2886ce1012635b64c9855c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 18:28:03 +0200

    Update Dabcaps.php

--- Dabcaps.php
+++ Dabcaps.php
@@ -96,7 +96,7 @@ href="cannacaps.html" />
             <div class="container">
             <div class="topbar-content">
             <div class="top-bar-left"> 
-            <span> <?php echo $_GET['username']; ?></span><span> </span><span>[Log out]</span>
+            <span> <?php echo $_GET['username']; ?></span><span> </span><span>[<a href="Logout.php?&auth=<?php echo $_GET['auth'];?>"><span style="color:red;"> Log out</span></a>]</span>
             </div>
             <div class="top-bar-right">
             <div class="topbar-menu">
