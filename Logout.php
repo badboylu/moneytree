86037630cbd0f3c03349135180838c661eb6d35c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 10:02:38 +0200

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,3 +1,31 @@
 <?php
-$sql="DELETE FROM pwdReset WHERE pwdResetEmail=?";
+
+   $servername="us-cdbr-east-02.cleardb.com";
+   $dBUsername="b7fcd41c893d7a";
+   $dBPassword="1e8f896b7da9e41";
+   $dBName="heroku_61db5a5cdc2dfd8";
+   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+
+ authtoken= $_GET['auth'];
+ $sql="DELETE FROM userauth2 WHERE token=?";
+ $stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.html?error=SQL1");
+       exit();
+}
+   mysqli_stmt_bind_param($stmt,"ss",$authtoken);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $count=mysqli_stmt_num_rows($stmt);
+   mysqli_stmt_close($stmt);
+if ($count>0){
+    header("Location:Signin.php?error=notworking");
+    exit();
+}
+else{
+    header("Location:Signin.php?logout=successful");
+    exit();
+}
+
+
 ?>
