88d3d6e4f0a00046a3ed9f0272583276148b1feb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 15:20:29 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -1,14 +1,11 @@
 <?php
-session_start();
-$_SESSION['username'] = $_GET['username'];
-$servername="us-cdbr-east-02.cleardb.com";
-$dBUsername="b7fcd41c893d7a";
-$dBPassword="1e8f896b7da9e41";
-$dBName="heroku_61db5a5cdc2dfd8";
-$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-
+   $servername="us-cdbr-east-02.cleardb.com";
+   $dBUsername="b7fcd41c893d7a";
+   $dBPassword="1e8f896b7da9e41";
+   $dBName="heroku_61db5a5cdc2dfd8";
+   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
    $authtoken = $_GET['auth'];
-   $sql='SELECT * FROM userauth2 WHERE token="?"';
+   $sql='SELECT * FROM userauth2 WHERE token=?';
    $stmt=mysqli_stmt_init($conn);
  if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:Signin.php?error=SQL1");
