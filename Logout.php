2a03587bbea3b0ad611edcf7a5113adc123930cc
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 10:08:58 +0200

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -6,7 +6,7 @@
    $dBName="heroku_61db5a5cdc2dfd8";
    $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 
- authtoken= $_GET['auth'];
+ $authtoken= $_GET['auth'];
  $sql="DELETE FROM userauth2 WHERE token=?";
  $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
