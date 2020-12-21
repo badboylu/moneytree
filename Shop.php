ff4e1ebc641f5f15ac7caf21f3a18345640980af
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-21 09:27:41 +0200

    Update Shop.php

--- Shop.php
+++ Shop.php
@@ -1,4 +1,10 @@
 <?php
+   session_start();
+   $servername="us-cdbr-east-02.cleardb.com";
+   $dBUsername="b7fcd41c893d7a";
+   $dBPassword="1e8f896b7da9e41";
+   $dBName="heroku_61db5a5cdc2dfd8";
+   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
    $authtoken = $_GET['auth'];
    $sql='SELECT * FROM userauth2 WHERE token="?"';
    $stmt=mysqli_stmt_init($conn);
