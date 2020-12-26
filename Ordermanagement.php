55fce4b7814963222ce596fa680d95ec1ea7bd7d
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-26 09:24:35 +0200

    Update Ordermanagement.php

--- Ordermanagement.php
+++ Ordermanagement.php
@@ -3,6 +3,13 @@
 echo '<script type="text/JavaScript">  
 
      var orderDP = localStorage.getItem('GramsDP'); 
+     var orderOC = localStorage.getItem('GramsOC'); 
+     var orderBW = localStorage.getItem('GramsBW'); 
+     var orderNL = localStorage.getItem('GramsNL'); 
+     var orderPR = localStorage.getItem('GramsPR'); 
+     var orderCC = localStorage.getItem('GramsCC'); 
+     var orderCCES = localStorage.getItem('GramsCCES'); 
+     var orderPE = localStorage.getItem('GramsPE'); 
 
      </script>' 
 ; 
