2cdd8e2036ff2176e0f814e1b6b1aad8b3c0588b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-27 19:38:55 +0200

    Update Ordermanagement.php

--- Ordermanagement.php
+++ Ordermanagement.php
@@ -6,20 +6,24 @@ $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 ?>
 <html>
 <form action="https://canibuy.co.za/Ordermanagement.php" method="get" id="OrderForm" name="OrderForm">
+<input type="hidden" name="DPgrams" id="DPgrams" value="">
+<input type="hidden" name="OCgrams" id="OCgrams" value="">
+<input type="hidden" name="CCgrams" id="CCgrams" value="">
+<input type="hidden" name="CCESgrams" id="CCESgrams" value="">
+<input type="hidden" name="BWgrams" id="BWgrams" value="">
 <input type="hidden" name="NLgrams" id="NLgrams" value="">
+<input type="hidden" name="PEgrams" id="PEgrams" value="">
 </form> 
 <script type="text/JavaScript">  
-     var orderDP = localStorage.getItem('GramsDP'); 
-     var orderOC = localStorage.getItem('GramsOC'); 
-     var orderBW = localStorage.getItem('GramsBW'); 
-     var orderNL = localStorage.getItem('GramsNL'); 
-     var orderPR = localStorage.getItem('GramsPR'); 
-     var orderCC = localStorage.getItem('GramsCC'); 
-     var orderCCES = localStorage.getItem('GramsCCES'); 
-     var orderPE = localStorage.getItem('GramsPE'); 
-
- if(orderNL == null){
+ if(orderNL == null || orderDP == null || orderOC == null || orderBW == null || orderCC == null || orderCCES == null || orderPR == null || orderPE == null ){
   orderNL = 0;
+  orderDP = 0;
+  orderOC = 0;
+  orderCC = 0;
+  orderBW = 0;
+  orderPR = 0;
+  orderCC = 0;
+  orderCCES = 0;
   document.getElementById("NLgrams").value = orderNL;
   document.getElementById("DPgrams").value = orderDP;
   document.getElementById("OCgrams").value = orderOC;
@@ -30,6 +34,14 @@ $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
   document.getElementById("CCESgrams").value = orderCCES;
   document.getElementById("OrderForm").submit();
 }else{
+  orderDP = localStorage.getItem('GramsDP'); 
+  orderOC = localStorage.getItem('GramsOC'); 
+  orderBW = localStorage.getItem('GramsBW'); 
+  orderNL = localStorage.getItem('GramsNL'); 
+  orderPR = localStorage.getItem('GramsPR'); 
+  orderCC = localStorage.getItem('BatchCC'); 
+  orderCCES = localStorage.getItem('BatchCCES'); 
+  orderPE = localStorage.getItem('GramsPE'); 
   document.getElementById("NLgrams").value = orderNL;
   document.getElementById("DPgrams").value = orderDP;
   document.getElementById("OCgrams").value = orderOC;
