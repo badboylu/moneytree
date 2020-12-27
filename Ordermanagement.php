c49e97a739fa7ceac992ee9727d1566ddd18ea69
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-28 00:47:22 +0200

    Update Ordermanagement.php

--- Ordermanagement.php
+++ Ordermanagement.php
@@ -7,53 +7,13 @@ $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 <html>
 <form action="https://canibuy.co.za/Ordermanagement.php" method="get" id="OrderForm" name="OrderForm">
 <input type="hidden" name="DPgrams" id="DPgrams" value="">
-<input type="hidden" name="OCgrams" id="OCgrams" value="">
-<input type="hidden" name="CCgrams" id="CCgrams" value="">
-<input type="hidden" name="CCESgrams" id="CCESgrams" value="">
-<input type="hidden" name="BWgrams" id="BWgrams" value="">
-<input type="hidden" name="NLgrams" id="NLgrams" value="">
-<input type="hidden" name="PEgrams" id="PEgrams" value="">
-</form> 
-<script type="text/JavaScript">  
- if(orderNL == null || orderDP == null || orderOC == null || orderBW == null || orderCC == null || orderCCES == null || orderPR == null || orderPE == null ){
+</form>
+if(orderNL == null){
   orderNL = 0;
-  orderDP = 0;
-  orderOC = 0;
-  orderCC = 0;
-  orderBW = 0;
-  orderPR = 0;
-  orderCC = 0;
-  orderCCES = 0;
   document.getElementById("NLgrams").value = orderNL;
-  document.getElementById("DPgrams").value = orderDP;
-  document.getElementById("OCgrams").value = orderOC;
-  document.getElementById("CCgrams").value = orderCC;
-  document.getElementById("BWgrams").value = orderBW;
-  document.getElementById("PRgrams").value = orderPR;
-  document.getElementById("PEgrams").value = orderPE;
-  document.getElementById("CCESgrams").value = orderCCES;
   document.getElementById("OrderForm").submit();
-}else{
-  orderDP = localStorage.getItem('GramsDP'); 
-  orderOC = localStorage.getItem('GramsOC'); 
-  orderBW = localStorage.getItem('GramsBW'); 
-  orderNL = localStorage.getItem('GramsNL'); 
-  orderPR = localStorage.getItem('GramsPR'); 
-  orderCC = localStorage.getItem('BatchCC'); 
-  orderCCES = localStorage.getItem('BatchCCES'); 
-  orderPE = localStorage.getItem('GramsPE'); 
+}else if(orderNL){
+  orderNL = localStorage.getItem('GramsNL');
   document.getElementById("NLgrams").value = orderNL;
-  document.getElementById("DPgrams").value = orderDP;
-  document.getElementById("OCgrams").value = orderOC;
-  document.getElementById("CCgrams").value = orderCC;
-  document.getElementById("BWgrams").value = orderBW;
-  document.getElementById("PRgrams").value = orderPR;
-  document.getElementById("PEgrams").value = orderPE;
-  document.getElementById("CCESgrams").value = orderCCES;
   document.getElementById("OrderForm").submit();
 }
-</script>
-</html>
-
-
-
