b7825b9c9e1f4cdfacfb2afcad0afb356e785abe
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-28 09:30:58 +0200

    Update Ordermanagement.php

--- Ordermanagement.php
+++ Ordermanagement.php
@@ -8,8 +8,12 @@ $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 <form action="https://canibuy.co.za/Ordermanagement.php" method="get" id="OrderForm" name="OrderForm">
 <input type="hidden" name="NLgrams" id="NLgrams" value="">
 <input type="hidden" name="DPgrams" id="DPgrams" value="">
+<input type="hidden" name="BWgrams" id="BWgrams" value="">
+<input type="hidden" name="OCgrams" id="OCgrams" value="">
 </form>
 <script type="text/JavaScript"> 
+var  orderNL = localStorage.getItem('GramsNL');
+var  orderDP = localStorage.getItem('GramsDP');
 var  orderNL = localStorage.getItem('GramsNL');
 var  orderDP = localStorage.getItem('GramsDP');
 if(orderNL == null){
@@ -26,6 +30,22 @@ if(orderDP == null){
   document.getElementById("OrderForm").submit();
 }else if(orderDP){
   document.getElementById("DPgrams").value = orderDP;
+  document.getElementById("OrderForm").submit();
+}
+if(orderBW == null){
+  orderBW = 0;
+  document.getElementById("BWgrams").value = orderBW;
+  document.getElementById("OrderForm").submit();
+}else if(orderBW){
+  document.getElementById("BWgrams").value = orderBW;
+  document.getElementById("OrderForm").submit();
+}
+if(orderOC == null){
+  orderOC = 0;
+  document.getElementById("OCgrams").value = orderOC;
+  document.getElementById("OrderForm").submit();
+}else if(orderOC){
+  document.getElementById("OCgrams").value = orderOC;
   document.getElementById("OrderForm").submit();
 }
 </script>
