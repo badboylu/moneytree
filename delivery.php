98362c024a6eb165c2abda314744803823da7134
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-28 16:12:37 +0200

    Update delivery.php

--- delivery.php
+++ delivery.php
@@ -1,4 +1,5 @@
-t  $servername="us-cdbr-east-02.cleardb.com";
+<?php
+   $servername="us-cdbr-east-02.cleardb.com";
    $dBUsername="b7fcd41c893d7a";
    $dBPassword="1e8f896b7da9e41";
    $dBName="heroku_61db5a5cdc2dfd8";
