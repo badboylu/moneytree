2610ce6b93653b906a1cf13ae3b7d752453b06db
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-30 01:08:53 +0200

    Update Orderget.php

--- Orderget.php
+++ Orderget.php
@@ -23,3 +23,48 @@ foreach( $pfData as $key => $val ) {
 }
 
 $pfParamString = substr( $pfParamString, 0, -1 );
+
+function pfValidSignature( $pfData, $pfParamString, $pfPassphrase = null ) {
+    // Calculate security signature
+    if($pfPassphrase === null) {
+        $tempParamString = $pfParamString;
+    } else {
+        $tempParamString = $pfParamString.'&passphrase='.urlencode( $pfPassphrase );
+    }
+
+    $signature = md5( $tempParamString );
+    return ( $pfData['signature'] === $signature );
+}
+
+function pfValidIP() {
+    // Variable initialization
+    $validHosts = array(
+        'www.payfast.co.za',
+        'sandbox.payfast.co.za',
+        'w1w.payfast.co.za',
+        'w2w.payfast.co.za',
+        );
+
+    $validIps = [];
+
+    foreach( $validHosts as $pfHostname ) {
+        $ips = gethostbynamel( $pfHostname );
+
+        if( $ips !== false )
+            $validIps = array_merge( $validIps, $ips );
+    }
+
+    // Remove duplicates
+    $validIps = array_unique( $validIps );
+    $referrerIp = gethostbyname(parse_url($_SERVER['HTTP_REFERER'])['host']);
+    if( in_array( $referrerIp, $validIps, true ) ) {
+        return true;
+    }
+    return false;
+}
+
+function pfValidPaymentData( $cartTotal, $pfData ) {
+    return !(abs((float)$cartTotal - (float)$pfData['amount_gross']) > 0.01);
+}
+
+?>
