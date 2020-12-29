e94c541c3e2f8941cd795a9c38bbd60dbf0e012d
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-29 23:13:38 +0200

    Update Orderpost.php

--- Orderpost.php
+++ Orderpost.php
@@ -1,19 +1,17 @@
 <?php
 $cartTotal = 10.00;
 $data = array(
-    'merchant_id' => '10000100',
-    'merchant_key' => '46f0cd694581a',
-    'return_url' => 'http://www.yourdomain.co.za/return.php',
-    'cancel_url' => 'http://www.yourdomain.co.za/cancel.php',
-    'notify_url' => 'http://www.yourdomain.co.za/notify.php',
-    // Buyer details
-    'name_first' => 'First Name',
-    'name_last'  => 'Last Name',
-    'email_address'=> 'test@test.com',
-    // Transaction details
-    'm_payment_id' => '1234', 
-    'amount' => '456',
-    'item_name' => 'Order#123'
+    'merchant_id' = '10000100',
+    'merchant_key' = '46f0cd694581a',
+    'return_url' = 'http://www.yourdomain.co.za/return.php',
+    'cancel_url' = 'http://www.yourdomain.co.za/cancel.php',
+    'notify_url' = 'http://www.yourdomain.co.za/notify.php',
+    'name_first' = 'First Name',
+    'name_last'  = 'Last Name',
+    'email_address'= 'test@test.com',
+    'm_payment_id' = '1234', 
+    'amount' = '456',
+    'item_name' = 'Order#123'
 );
 
 $signature = generateSignature($data);
