c75f5362d24f944da0e89bfac1818581f80cc80a
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-30 00:52:45 +0200

    Update Orderpost.php

--- Orderpost.php
+++ Orderpost.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * @param array $data
+ * @param null $passPhrase
+ * @return string
+ */
+function generateSignature($data, $passPhrase = null) {
+    // Create parameter string
+    $pfOutput = '';
+    foreach( $data as $key => $val ) {
+        if(!empty($val)) {
+            $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
+        }
+    }
+    // Remove last ampersand
+    $getString = substr( $pfOutput, 0, -1 );
+    if( $passPhrase !== null ) {
+        $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
+    }
+    return md5( $getString );
+}
+?>
+<?php
 $cartTotal = 10.00;
 $data = array(
     'merchant_id' => '10000100',
