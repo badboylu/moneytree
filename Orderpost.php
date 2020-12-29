009edcc4b87a01203939626f6de365cc5e883aea
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-30 00:27:17 +0200

    Update Orderpost.php

--- Orderpost.php
+++ Orderpost.php
@@ -12,8 +12,7 @@ $data = array(
     'm_payment_id' => '1234', 
     'amount' => '456',
     'item_name' => 'Order#123',
-    $signature = generateSignature($data);
-    $data['signature'] = $signature;
+    
 );
 $htmlForm = '<form action="https://sandbox.payfast.co.za/eng/process" method="post">';
 foreach($data as $name=> $value)
