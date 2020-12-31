b19838931da52b97655ec19bf90ea3f55de2b625
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-31 21:00:00 +0200

    Update Paymentprocess.php

--- Paymentprocess.php
+++ Paymentprocess.php
@@ -38,8 +38,5 @@ foreach($data as $name=> $value)
     $htmlForm .= '<input name="'.$name.'" type="hidden" value="'.$value.'" />';
 }
 $htmlForm .= '<input type="submit" name="Pay" value="Order" id="Blanks" class="Orderbtn" /></form>';
-echo'
-<script>
-document.getElementById("form").submit();
-</script>';
+echo $htmlForm;
 ?>
