954f109ecc7041686a7d3fad2b965916abc94e11
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2020-12-31 17:08:43 +0200

    Update Orderpost.php

--- Orderpost.php
+++ Orderpost.php
@@ -20,19 +20,6 @@
     }
     return md5( $getString );
 }
-if(isset($_POST['Pay']))
-{
-    $sql="INSERT INTO userauth2 (username,token) VALUES (?,?);";
-    $stmt= mysqli_stmt_init($conn);
-
-if(!mysqli_stmt_prepare($stmt, $sql)){
-    header("Location:Signin.php?error=sql007");
-    exit();
- }
-
- mysqli_stmt_bind_param($stmt,"ss",$username,$signature);
- mysqli_stmt_execute($stmt);
-}
 ?>
 <?php
 $cartTotal = 10.00;
@@ -51,6 +38,19 @@ $data = array(
 );
 $signature = generateSignature($data);
 $data['signature'] = $signature;
+if(isset($_POST['Pay']))
+{
+    $sql="INSERT INTO userauth2 (username,token) VALUES (?,?);";
+    $stmt= mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql)){
+    header("Location:Signin.php?error=sql007");
+    exit();
+ }
+
+ mysqli_stmt_bind_param($stmt,"ss",$username,$signature);
+ mysqli_stmt_execute($stmt);
+}
 ?>
 <!DOCTYPE html> <!--[if IE 8]><html class="ie ie8" lang="en-US"> <![endif]--> <!--[if !(IE 7) & !(IE 8)]><!--><html lang="en-US"> <!--<![endif]-->
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
