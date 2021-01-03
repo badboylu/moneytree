85d02e3c7c3f6a7861364ac6862feab920499997
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-03 17:35:45 +0200

    Update Paymentprocess.php

--- Paymentprocess.php
+++ Paymentprocess.php
@@ -16,7 +16,14 @@ $cces = $_GET['CCESgrams'];
 $cc = $_GET['CCgrams'];
 $pe = $_GET['PEgrams'];
 $address = 'Testing';
-
+$sql="INSERT INTO orders (idOrderNumber, idUsers, idOrderDPgrams, idOrderCCbatches, idOrderOCgrams, idOrderBWgrams, idOrderPRjays, idOrderNLgrams, idOrderCCESbatches, idOrderPEgrams, idOrderAddress) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.php?error=SQL3");
+       exit();
+}
+     mysqli_stmt_bind_param($stmt,"sssssssssss",$authtoken,$confirmedpayment,$dp,$cc,$oc,$bw,$pr,$nl,$cces,$pe,$address);
+     mysqli_stmt_execute($stmt);
 
    function generateSignature($data, $passPhrase = null) {
     // Create parameter string
