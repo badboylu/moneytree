0567707ceac4a8965d297753d4e79b9eef2ff6fa
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-03 07:25:54 +0200

    Update signuptest.php

--- signuptest.php
+++ signuptest.php
@@ -13,7 +13,7 @@ $passwordrepeat=$_POST['pwdrepeat'];
 $sql="SELECT uidUsers FROM users WHERE uidUsers=?";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.html?error=SQL1");
+       header("Location:register.php?error=SQL1");
        exit();
 }
 mysqli_stmt_bind_param($stmt,"s",$username);
@@ -21,18 +21,18 @@ mysqli_stmt_bind_param($stmt,"s",$username);
    mysqli_stmt_store_result($stmt);
    $resultCheck=mysqli_stmt_num_rows($stmt);
    if($resultCheck > 0){
-       header("Location:register.html?error=usernameexists");
+       header("Location:register.php?error=usernameexists");
        exit();
 } 
 
 $sql="SELECT emailUsers FROM users WHERE emailUsers=?";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.html?error=SQL2");
+       header("Location:register.php?error=SQL2");
        exit();
 }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       header("Location:register.html?error=invalidemail"."&username=".$username);
+       header("Location:register.php?error=invalidemail"."&username=".$username);
        exit();
 }
    mysqli_stmt_bind_param($stmt,"s",$email);
@@ -40,11 +40,11 @@ $stmt=mysqli_stmt_init($conn);
    mysqli_stmt_store_result($stmt);
    $resultCheck=mysqli_stmt_num_rows($stmt);
    if($resultCheck > 0){
-       header("Location:register.html?error=emailexists"."&username=".$username);
+       header("Location:register.php?error=emailexists"."&username=".$username);
        exit();
 } 
    if($_POST['pwd'] != $_POST['pwdrepeat']){
-       header("Location:register.html?error=pwdnomatch"."&username=".$username."&email=".$email);
+       header("Location:register.php?error=pwdnomatch"."&username=".$username."&email=".$email);
        exit();
 }
 
@@ -54,14 +54,14 @@ $stmt=mysqli_stmt_init($conn);
      $specialChars = preg_match('@[^\w]@', $password);
 
    if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
-       header("Location:register.html?error=invalidpwd"."&username=".$username."&email=".$email);
+       header("Location:register.php?error=invalidpwd"."&username=".$username."&email=".$email);
        exit();
 }
 
     $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
     $stmt=mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.html?error=SQL3");
+       header("Location:register.php?error=SQL3");
        exit();
 }
      $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
