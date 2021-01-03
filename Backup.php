e52a0a561840ad0c064834d389b123d49e88bc9c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-03 17:12:09 +0200

    Update Backup.php

--- Backup.php
+++ Backup.php
@@ -1,59 +1,19 @@
-<?php 
-header( 'HTTP/1.0 200 OK' );
-flush();
-define( 'SANDBOX_MODE', true );
-$pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
-?>
-<html>
-<form action="https://canibuy.co.za/Ordermanagement.php" method="get" id="OrderForm" name="OrderForm">
-<input type="hidden" name="DPgrams" id="DPgrams" value="">
-<input type="hidden" name="OCgrams" id="OCgrams" value="">
-<input type="hidden" name="CCgrams" id="CCgrams" value="">
-<input type="hidden" name="CCESgrams" id="CCESgrams" value="">
-<input type="hidden" name="BWgrams" id="BWgrams" value="">
-<input type="hidden" name="NLgrams" id="NLgrams" value="">
-<input type="hidden" name="PEgrams" id="PEgrams" value="">
-</form> 
-<script type="text/JavaScript">  
- if(orderNL == null || orderDP == null || orderOC == null || orderBW == null || orderCC == null || orderCCES == null || orderPR == null || orderPE == null ){
-  orderNL = 0;
-  orderDP = 0;
-  orderOC = 0;
-  orderCC = 0;
-  orderBW = 0;
-  orderPR = 0;
-  orderCC = 0;
-  orderCCES = 0;
-  document.getElementById("NLgrams").value = orderNL;
-  document.getElementById("DPgrams").value = orderDP;
-  document.getElementById("OCgrams").value = orderOC;
-  document.getElementById("CCgrams").value = orderCC;
-  document.getElementById("BWgrams").value = orderBW;
-  document.getElementById("PRgrams").value = orderPR;
-  document.getElementById("PEgrams").value = orderPE;
-  document.getElementById("CCESgrams").value = orderCCES;
-  document.getElementById("OrderForm").submit();
-}else{
-  orderDP = localStorage.getItem('GramsDP'); 
-  orderOC = localStorage.getItem('GramsOC'); 
-  orderBW = localStorage.getItem('GramsBW'); 
-  orderNL = localStorage.getItem('GramsNL'); 
-  orderPR = localStorage.getItem('GramsPR'); 
-  orderCC = localStorage.getItem('BatchCC'); 
-  orderCCES = localStorage.getItem('BatchCCES'); 
-  orderPE = localStorage.getItem('GramsPE'); 
-  document.getElementById("NLgrams").value = orderNL;
-  document.getElementById("DPgrams").value = orderDP;
-  document.getElementById("OCgrams").value = orderOC;
-  document.getElementById("CCgrams").value = orderCC;
-  document.getElementById("BWgrams").value = orderBW;
-  document.getElementById("PRgrams").value = orderPR;
-  document.getElementById("PEgrams").value = orderPE;
-  document.getElementById("CCESgrams").value = orderCCES;
-  document.getElementById("OrderForm").submit();
+$authtoken = $_GET['auth'];
+$confirmedpayment = 'N';
+$dp = $_GET['DPgrams'];
+$oc = $_GET['OCgrans'];
+$bw = $_GET['BWgrams'];
+$pr = $_GET['PRgrams'];
+$nl = $_GET['NLgrams'];
+$cces = $_GET['CCESgrams'];
+$cc = $_GET['CCgrams'];
+$pe = $_GET['PEgrams'];
+$address = $_GET['Province']',' $_GET['City']',' $_GET['TownPTA']',' $_GET['suburbsPTAeast']',' $_GET['Crib']',' $_GET['Estate']',' $_GET['Complex']',' $_GET['Building'];
+$sql="INSERT INTO orders (idOrderNumber, idUsers, idOrderDPgrams, idOrderCCbatches, idOrderOCgrams, idOrderBWgrams, idOrderPRjays, idOrderNLgrams, idOrderCCESbatches, idOrderPEgrams, idOrderAddress) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:register.php?error=SQL3");
+       exit();
 }
-</script>
-</html>
-
-
-
+     mysqli_stmt_bind_param($stmt,"sssssssssss",$authtoken,$confirmedpayment,$dp,$cc,$oc,$bw,$pr,$nl,$cces,$pe,$address);
+     mysqli_stmt_execute($stmt);
