d74887f00d9a09f06e6e28005e44067b0ea9cb87
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-05 06:57:08 +0200

    Update Paymentprocess.php

--- Paymentprocess.php
+++ Paymentprocess.php
@@ -1,9 +1,9 @@
 <?php
 $servername="us-cdbr-east-02.cleardb.com";
-$dBUsername="b7fcd41c893d7a";
-$dBPassword="1e8f896b7da9e41";
-$dBName="heroku_61db5a5cdc2dfd8";
-$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+   $dBUsername="b7fcd41c893d7a";
+   $dBPassword="1e8f896b7da9e41";
+   $dBName="heroku_61db5a5cdc2dfd8";
+   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $date = date("Y-m-d");
 $time = date("H:i:s");
 date_default_timezone_set('Africa/Johannesburg');
