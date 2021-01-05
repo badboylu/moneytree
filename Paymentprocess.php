d3a27c98d13d1e0eadb36f90037344fde9d71d05
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-05 09:47:05 +0200

    Update Paymentprocess.php

--- Paymentprocess.php
+++ Paymentprocess.php
@@ -46,15 +46,16 @@ $stmt=mysqli_stmt_init($conn);
      mysqli_stmt_execute($stmt);
      mysqli_stmt_close($stmt);
 
-$sql="INSERT INTO oders (idOrderUsername,idOrderDPgrams,idOrderCCbatches,idOrderOCgrams,idOrderBWgrams, idOrderPRjays, idOrderNLgrams, idOrderCCESbatches, idOrderPEgrams, idOrderConfirmation, idOrderToken, idOrderTime, idOrderDate) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+$sql="INSERT INTO address (idUsername,idProvince,idCity,idTown,idSuburb,idCrib,idBuilding,idEstate,idComplex,idMall,idShop,idStreet,idUnitNumber,idHouseNumber,idContactNumber,idTime,idDate,idToken,idOrderConfirmation) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 $stmt=mysqli_stmt_init($conn);
 
     if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:delivery.php?error=SQL6");
        exit();
 }
-     mysqli_stmt_bind_param($stmt,"siiiiiiiissss",$username,$dp,$cc,$oc,$bw,$pr,$nl,$cces,$pe,$confirmedpayment,$authtoken,$time,$date);
+     mysqli_stmt_bind_param($stmt,"sssssssssssssssssss",$username,$dp,$cc,$oc,$bw,$pr,$nl,$cces,$pe,$confirmedpayment,$authtoken,$time,$date);
      mysqli_stmt_execute($stmt);
+     mysqli_stmt_close($stmt);
 
    function generateSignature($data, $passPhrase = null) {
     // Create parameter string
