f1539dff6837e039dc1d0bf10b5343da3bd2cf33
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-06 18:22:24 +0200

    Update Distro.php

--- Distro.php
+++ Distro.php
@@ -4,7 +4,7 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$query="select * from oders";
+$query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
 while($row=mysqli_fetch_array($result)){
  $ordernumbers[]=$row['idOrders'];
