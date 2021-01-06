43118544a2926b932f98b09cff7db7617bac69ab
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-06 11:53:10 +0200

    Update Driver.php

--- Driver.php
+++ Driver.php
@@ -48,8 +48,7 @@ $result=mysqli_query($conn,$query);
     <div class="page-main-header">
         <div class="main-header-right row">
             <div class="main-header-left d-lg-none">
-                <div class="logo-wrapper"><a href="index.html"><img class="blur-up lazyloaded" src="multikart-logo.png" alt=""></a></div>
-            </div>
+                
             <div class="mobile-sidebar">
                 <div class="media-body text-right switch-sm">
                     <label class="switch"><a href="#"><i id="sidebar-toggle" data-feather="align-left"></i></a></label>
