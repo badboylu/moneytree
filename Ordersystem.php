a320fd42ad17d3847956acf81b489431f9850504
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-07 06:59:44 -0800

    Update Ordersystem.php

--- Ordersystem.php
+++ Ordersystem.php
@@ -4,5 +4,21 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$query="select * from address ";
+$query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
+$OrderReady="Prepared";
+while($row=mysqli_fetch_array($result)){
+ $ordernumbers[]=$row['idOrders'];
+}
+$order=Min($ordernumbers);
+$sql="INSERT INTO oders (idOrderConfirmation) WHERE idOrders=".$order" VALUES (?);";
+    $stmt= mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($stmt, $sql)){
+    header("Location:Distro.php?error=sqlerror777");
+    exit();
+ }
+ mysqli_stmt_bind_param($stmt,"s",$OrderReady);
+ mysqli_stmt_execute($stmt);
+ 
+    header("Location:Distro.php?login=successful"."&auth=".$token."&username=".$uid);
+    exit();
