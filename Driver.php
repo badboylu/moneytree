3780fc31caa732e9527272f7a4996492cf3677af
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-09 00:25:43 +0200

    Update Driver.php

--- Driver.php
+++ Driver.php
@@ -209,7 +209,7 @@ else{
                          <br>
                          </div>
                          <form action="Ordersystemdelivery.php" method="post" id="form">
-                         <input type="submit" name="Delivered" value="Delivered" id="Blanks" "/>
+                         <input type="submit" name="Delivered" value="Deliver" id="Blanks" "/>
                          </form>
                         
                     </div>
