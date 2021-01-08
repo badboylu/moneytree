84fc8e5f4828cb444a94311e30d6203cb4b46f7c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-08 09:41:09 +0200

    Update Next.php

--- Next.php
+++ Next.php
@@ -7,7 +7,6 @@ $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
 $count=mysqli_num_rows($result);
-mysqli_stmt_close($stmt);
 if (!$count>0){
     header("Location:D2.php");
     exit();
