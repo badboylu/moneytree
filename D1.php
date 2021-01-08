5c00ca67e19997655191182fb08482db85517f6e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-08 13:56:37 +0200

    Update D1.php

--- D1.php
+++ D1.php
@@ -4,6 +4,13 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
+$result=mysqli_query($conn,$query);
+$count=mysqli_num_rows($result);
+   if (!$count){
+         header("Location:D2.php");
+         exit();
+   }
 ?>
 <html lang="en">
 <head>
