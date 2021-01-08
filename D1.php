c057265eaedb679190151a96a39ad7e079e2889b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-08 11:10:27 +0200

    Update D1.php

--- D1.php
+++ D1.php
@@ -4,16 +4,6 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
-$result=mysqli_query($conn,$query);
-while($row=mysqli_fetch_array($result)){
- $ordernumbers[]=$row['idOrders'];
-}
-$order=Min($ordernumbers);
-$final="SELECT * FROM oders WHERE idOrders=".$order;
-$finalresult=mysqli_query($conn,$final);
-$row = mysqli_fetch_array($finalresult);
-$count=mysqli_num_rows($result);
 ?>
 <html lang="en">
 <head>
