1d4177c2da01a314a4df00bde67dbaebc7b4e020
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-08 11:58:06 +0200

    Update D2.php

--- D2.php
+++ D2.php
@@ -234,15 +234,6 @@ $count=mysqli_num_rows($result);
 <script>
 function hideOrder(){
 	
-var DP = <?php echo $row['idOrderDPgrams'] ?>;
-var CC = <?php echo $row['idOrderCCbatches']?>;
-var OC = <?php echo $row['idOrderOCgrams']?>;
-var BW = <?php echo $row['idOrderBWgrams']?>;
-var PR = <?php echo $row['idOrderPRjays']?>;
-var CCES = <?php echo $row['idOrderCCESbatches']?>;
-var NL = <?php echo $row['idOrderNLgrams']?>;
-var PE = <?php echo $row['idOrderPEgrams']?>;
-	
 
    document.getElementById("hideDP").style.display = "none";
    document.getElementById("hideCC").style.display = "none";
