aace5bf342b4c86c020dfda50a4b488472d201b6
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-09 17:09:16 +0200

    Update Payfastpost.php

--- Payfastpost.php
+++ Payfastpost.php
@@ -4,6 +4,6 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$row=$_POST
+$row=$_POST['ITN_Payload']
 $ordernumbers[]=$row;
 echo $ordernumbers;
