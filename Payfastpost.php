7c01a31667e395f85fa4ba714b1273dd142b740e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-09 17:20:21 +0200

    Update Payfastpost.php

--- Payfastpost.php
+++ Payfastpost.php
@@ -4,7 +4,6 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$row=$_POST
 $emails = array();
 
 foreach ( $_POST as $key => $value )
