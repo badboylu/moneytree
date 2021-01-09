65b3722021ffa1070c79d8428d33535ba5fff6a2
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-09 16:11:23 +0200

    Create Payfastpost.php

---
+++ Payfastpost.php
@@ -0,0 +1,7 @@
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="1e8f896b7da9e41";
+$dBName="heroku_61db5a5cdc2dfd8";
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$payload=$_POST;
+echo $payload;
