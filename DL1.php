e62ee9612650f8f963ee323a37cb02019a7dbf40
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-09 14:41:47 +0200

    Update DL1.php

--- DL1.php
+++ DL1.php
@@ -176,7 +176,7 @@ $count=mysqli_num_rows($result);
                          <br>
                          <br>
                          </div>
-                         <form action="Ordersystemdelivery.php" method="post" id="form">
+                         <form action="Driver.php" method="post" id="form">
                          <input type="submit" name="Next" value="Next order" id="Blanks" "/>
                          </form>
                     </div>
