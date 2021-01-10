0b4e7acb290bc7bfd62ac1e3b6807048652711b6
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-10 08:34:23 +0200

    Update Ordersystem.php

--- Ordersystem.php
+++ Ordersystem.php
@@ -21,7 +21,7 @@ while($row=mysqli_fetch_array($result1)){
  $ordernumbers1[]=$row['idUser'];
 }
 $order1=Min($ordernumbers1);
-$sql1="UPDATE oders SET idOrderConfirmation='".$orderready1."' WHERE idOrders='".$order1."'";
+$sql1="UPDATE address SET idOrderConfirmation='".$orderready1."' WHERE idOrders='".$order1."'";
 mysqli_query($conn,$sql1);
     header("Location:D1.php");
     exit();
