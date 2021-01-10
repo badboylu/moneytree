160bc6316dede7e12cd714559950659a6ea92abc
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-10 07:49:05 +0200

    Create AddressReset.php

---
+++ AddressReset.php
@@ -0,0 +1,8 @@
+<?php
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="1e8f896b7da9e41";
+$dBName="heroku_61db5a5cdc2dfd8";
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$sql="DELETE FROM address";
+mysqli_query($conn, $sql);
