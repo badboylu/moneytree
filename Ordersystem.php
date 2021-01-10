8e7d778484838ac8588c4d193d08c889efe7a587
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-10 10:20:25 +0200

    Update Ordersystem.php

--- Ordersystem.php
+++ Ordersystem.php
@@ -4,6 +4,7 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+
 $query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
 $orderready='Prepared';
@@ -11,20 +12,6 @@ while($row=mysqli_fetch_array($result)){
  $ordernumbers[]=$row['idOrders'];
 }
 $order=Min($ordernumbers);
-$sql="UPDATE oders SET idOrderConfirmation='".$orderready."' WHERE idOrders='".$order."'";
-mysqli_query($conn,$sql);
-
-
-$sql0="SELECT * FROM address WHERE idOrderConfirmation='Placed'";
-$result1=mysqli_query($conn,$sql0);
-
-$orderready1='Pending';
-
-while($row=mysqli_fetch_array($result1)){
- $ordernumbers1[]=$row['idUser'];
-}
-
-$order1=Max($ordernumbers1);
 
 $sql2="SELECT * FROM oders WHERE WHERE idOrders='".$order."'";
 $result2=mysqli_query($conn,$sql2);
@@ -33,5 +20,11 @@ while($row=mysqli_fetch_array($result2)){
 }
 
 echo $tokenarray;
+
+
+
+
+
+
 
 
