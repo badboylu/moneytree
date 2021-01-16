606b99243d78b85464e8f98f5ef1b6862380cc61
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-16 13:38:12 +0200

    Update DL01.php

--- DL01.php
+++ DL01.php
@@ -4,7 +4,7 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
+$query="SELECT * FROM oders WHERE idOrderConfirmation='Prepared'";
 $result=mysqli_query($conn,$query);
 $count=mysqli_num_rows($result);
    if ($count>0){
