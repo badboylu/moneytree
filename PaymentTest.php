e4edc19c6a8a435c4558cc3f144cf4ff682b0737
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-17 12:36:23 +0200

    Update PaymentTest.php

--- PaymentTest.php
+++ PaymentTest.php
@@ -7,20 +7,20 @@ function generateSignature($data, $passPhrase = null) {
             $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
         }
     }
-
+    // Remove last ampersand
     $getString = substr( $pfOutput, 0, -1 );
     if( $passPhrase !== null ) {
         $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
     }
     return md5( $getString );
-} 
+}
 
 $data = array(
     // Merchant details
     'merchant_id' => '10000100',
     'merchant_key' => '46f0cd694581a',
     'amount' => '100.00',
-    'item_name' => 'Test Product',
+    'item_name' => 'TestProduct',
     'return_url' => 'https://canibuy.co.za/Payfastpost.php',
     'cancel_url' => 'https://canibuy.co.za/Shopredirect.php',
     'notify_url' => 'https://canibuy.co.za/Payfastpost.php',
@@ -35,7 +35,7 @@ echo $signature;
    <input type="hidden" name="merchant_id" value="10000100">
    <input type="hidden" name="merchant_key" value="46f0cd694581a">
    <input type="hidden" name="amount" value="100.00">
-   <input type="hidden" name="item_name" value="Test Product">
+   <input type="hidden" name="item_name" value="TestProduct">
    <input type="hidden" name="return_url" value="https://canibuy.co.za/Payfastpost.php">
    <input type="hidden" name="cancel_url" value="https://canibuy.co.za/Shopredirect.php">
    <input type="hidden" name="notify_url" value="https://canibuy.co.za/Payfastpost.php">
