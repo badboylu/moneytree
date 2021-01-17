35521e1626ecfec7478afe387397efc157638abe
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-17 11:04:26 +0200

    Update Payfastpost.php

--- Payfastpost.php
+++ Payfastpost.php
@@ -2,34 +2,10 @@
 header( 'HTTP/1.0 200 OK' );
 flush();
 
-define( 'SANDBOX_MODE', true );
-$pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
-// Posted variables from ITN
 $pfData = $_POST;
 
-// Strip any slashes in data
-foreach( $pfData as $key => $val ) {
+foreach( $pfData as $key = $val ) {
     $pfData[$key] = stripslashes( $val );
 }
 
-// Convert posted variables to a string
-foreach( $pfData as $key => $val ) {
-    if( $key !== 'signature' ) {
-        $pfParamString .= $key .'='. urlencode( $val ) .'&';
-    } else {
-        break;
-    }
-}
-
-$pfParamString = substr( $pfParamString, 0, -1 ); 
-
-foreach ( $_POST as $key => $value )
-{
-    if ($key)
-    {
-        $this_email = $value;
-        $emails[] = $this_email;
-    }
-}
- 
-echo $emails;
+echo $pfData;
