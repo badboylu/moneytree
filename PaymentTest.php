5026adc4a9095263d1a1144c461b0711b50da3a4
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-17 13:08:16 +0200

    Update PaymentTest.php

--- PaymentTest.php
+++ PaymentTest.php
@@ -34,7 +34,7 @@ echo $signature;
 <form action="https://sandbox.payfast.co.za/eng/process?true" method="post">
    <input type="hidden" name="merchant_id" value="10000100">
    <input type="hidden" name="merchant_key" value="46f0cd694581a">
-   <input type="hidden" name="return_url" value="https://canibuy.co.za/Payfastpost.php">
+   <input type="hidden" name="return_url" value="https://canibuy.co.za/Shop.php">
    <input type="hidden" name="cancel_url" value="https://canibuy.co.za/Shopredirect.php">
    <input type="hidden" name="notify_url" value="https://canibuy.co.za/Payfastpost.php">
    <input type="hidden" name="amount" value="100.00">
