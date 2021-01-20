6257f34674e760bb22a6c0dda5b00ec8b41bbca9
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-20 10:08:45 +0200

    Update Distro.php

--- Distro.php
+++ Distro.php
@@ -40,7 +40,7 @@ else{
   echo '
   <html>
   <form action="D2.php" id="verify">
-  <input type="hidden" name="auth" value="'.<?php echo $_GET['auth'] ?>.'">
+  <input type="hidden" name="auth" value="<?php echo $_GET['auth'] ?>">
   <input type="hidden" name="username" value="<?php echo $_GET['username'] ?>">
   <input type="submit">
   </form>
@@ -48,7 +48,6 @@ else{
   document.getElementById("verify").submit();
   </script>
   </html> ';
-  exit();
 }
 ?>
 <html lang="en">
