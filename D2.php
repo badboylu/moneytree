a592dd5fbf4cd0f4003461b6fce63dec69a2b6da
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-20 10:26:31 +0200

    Update D2.php

--- D2.php
+++ D2.php
@@ -7,7 +7,10 @@ $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
 $count=mysqli_num_rows($result);
-   
+   if ($count>0){
+         header("Location:Distro.php");
+         exit();
+   }
 ?>
 <html lang="en">
 <head>
