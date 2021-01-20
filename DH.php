a25493f6114cf5aab01b918f5d91795705699e33
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-20 10:53:05 +0200

    Update DH.php

--- DH.php
+++ DH.php
@@ -98,7 +98,7 @@
                 </div>
                 <ul class="sidebar-menu">
                     <li><a class="sidebar-header" href="#"><i data-feather="home"></i><span>Dashboard</span></a></li>
-                    <li><a class="sidebar-header" href="#"><i data-feather="box"></i> <span>Order Preparation</span></a>
+                    <li><a class="sidebar-header" href="#"><i data-feather="box"></i><span>Order Preparation</span></a>
                         <ul class="sidebar-submenu">
                             <li>
                                 <a href="Distro.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>"><i class="fa fa-circle"></i>
