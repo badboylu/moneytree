001f9cf8b8d3161bf7760cb2c2b163940eb4db08
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-23 13:38:59 +0200

    Update A1.php

--- A1.php
+++ A1.php
@@ -220,7 +220,7 @@ Canibuy Disposal Agent Agreement
 Copyright © Canibuy, Johannesburg, South Africa. All rights not expressly granted, are reserved. To obtain permission for the use of the Canibuy site and the disposal of any product users must be 18 years of age or older.
 </p>
 <p>
-BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YOU (THE “Disposal Agent”) ARE INDICATING YOUR ACCEPTANCE TO BE BOUND BY THE TERMS AND CONDITIONS OF THIS DISPOSAL AGENT AGREEMENT are 18 years of age or older. 
+BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YOU (THE “Disposal Agent”) ARE INDICATING YOUR ACCEPTANCE TO BE BOUND BY THE TERMS AND CONDITIONS OF THIS DISPOSAL AGENT AGREEMENT and are 18 years of age or older. 
 </p>
 <p>
 <strong>
