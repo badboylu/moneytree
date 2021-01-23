999ebd756f117fe6c548e6a1337c2f602fc68147
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-23 13:23:29 +0200

    Update EA.php

--- EA.php
+++ EA.php
@@ -1,6 +1,8 @@
 <html>
 <h1>
+<strong>
 Canibuy Disposal Agent Agreement
+</strong>
 </h1>
 <p>
 Copyright © Canibuy, Johannesburg, South Africa. All rights not expressly granted, are reserved. To obtain permission for the use of the Canibuy site and the disposal of any product users must be 18 years of age or older.
@@ -9,7 +11,9 @@ Copyright © Canibuy, Johannesburg, South Africa. All rights not expressly grant
 BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YOU (THE “USER”) ARE INDICATING YOUR ACCEPTANCE TO BE BOUND BY THE TERMS AND CONDITIONS OF THIS DISPOSAL AGENT AGREEMENT.
 </p>
 <p>
+<strong>
 1. GENERAL
+</strong>
 </p>
 <p>
 1.1 This agreement herein may be changed without notice. Your continued use of this Site after any such changes constitutes your acceptance of the new end user agreement and its terms and conditions.
@@ -24,19 +28,25 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 1.4 No failure or delay by Canibuy to exercise any of its rights will be construed as a waiver of any such right, whether this is done expressly or implied, nor will it affect the validity of any part of this end user agreement or prejudice our right to take subsequent action against the User.
 </p>
 <p>
+<strong>
 2. COPYRIGHT
+</strong>
 </p>
 <p>
 2.1 This end user agreement applies exclusively to your access to, and use of, this Site and does not alter in any way, the terms or conditions of any other agreement you may have with Canibuy for the service of disposing of products or otherwise.
 </p>
 <p>
+<strong>
 3. INTELLECTUAL PROPERTY RIGHTS
+</strong>
 </p>
 <p>
 3.1 All the content, trademarks and data on this web site, including but not limited to, software, databases, text, graphics, icons, hyperlinks, private information, designs and agreements, are the property of, or are licensed to Canibuy and as such are protected from infringement by local and international legislation and treaties.
 </p>
 <p>
+<strong>
 4. LICENSES AND SITE ACCESS
+</strong>
 </p>
 <p>
 4.1 By registering with Canibuy, you are agree to become a Canibuy subcontractor and given the title of Disposal Agent.
@@ -48,13 +58,17 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 4.3 You may not use, frame or utilize framing techniques to enclose any Canibuy trademark, logo or other proprietary information (including the images found at this Site, the content of any text or the layout/design of any page or form contained on a page) without Canibuy‘s express written consent. Further, you may not use any meta tags or any other “hidden text” utilizing a Canibuy name, trademark or product name without Canibuy‘s express written consent.
 </p>
 <p>
+<strong>
 5. PRODUCT DISPOSAL
+</strong>
 </p>
 <p>
 5.1 Canibuy makes no claim or representation regarding, and accepts no responsibility for, the safe handling and disposal of any products by the Disposal Agent. The product disposal methods employed by Disposal Agents are not under the control of Canibuy and Canibuy is not responsible for the illegal use and or application of the products to be disposed of.
 </p>
 <p>
+<strong>
 6. DISPOSAL AGENT CONDUCT
+</strong>
 </p>
 <p>
 6.1.1 In using this Site, and in receiving products to dispose of, you agree:
@@ -69,19 +83,25 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 6.2 In addition, disposal agents agree that they are solely responsible for their actions of disposing products for Canibuy and that they will comply with all applicable local, national and international laws and regulations in this regard.
 </p>
 <p>
+<strong>
 7. INDEMNIFICATION
+</strong>
 </p>
 <p>
 7.1 Disposal Agents shall remain solely liable for the safe and legal disposal of any products delivered by Canibuy. You agree to indemnify and hold harmless Canibuy from any claim, action, demand, loss or damages (including attorneys’ fees) made or incurred by any third party arising out of or relating to your conduct, your violation of this Disposal Agent agreement, or your violation of any rights of a third party.
 </p>
 <p>
+<strong>
 8. SUBMISSIONS
+</strong>
 </p>
 <p>
 8.1 You agree that any materials, including but not limited to questions, comments, suggestions, ideas, plans, notes, drawings, original or creative materials or other information, provided by you in the form of email or submissions to Canibuy, or postings on this Site, are non-confidential and shall become the sole property of Canibuy. Canibuy shall own exclusive rights, including all intellectual property rights, and shall be entitled to the unrestricted use of these materials for any purpose, commercial or otherwise, without acknowledgment or compensation to you. The submission of any materials to Canibuy irrevocably waives any and all “moral rights” in such materials, including the rights of paternity and integrity.
 </p>
 <p>
+<strong>
 9. EMAIL DISCLAIMER
+</strong>
 </p>
 <p>
 9.1 The information in all e-mail messages (document and attachments) sent by an employee / electronic agent / a legal member of Canibuu is confidential and may be legally privileged. The information transmitted is intended only for the person(s) or entity to which it is addressed. If you are not the intended recipient(s), please notify the sender immediately and then delete this email (document and attachments). Do not disclose the contents of this email (document and attachments) to any other person, nor make any copies thereof.
@@ -93,13 +113,17 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 9.3 No employee is authorised to conclude a binding agreement on behalf of Canibuy by email without the express written confirmation of a director of Canibuy. Nothing contained in said email shall be construed as a legally binding agreement or an offer to contract.
 </p>
 <p>
+<strong>
 10. ACCEPTANCE OF FUNDS
+</strong>
 </p>
 <p>
 10.1 You acknowledge that (i) Canibuy is not offering any of the displayed products for sale and the Service you are providing for Canibuy is a waste disposal service of products rather the sale and use of the waste products, and (ii) any funds paid to Canibuy by the Disposal Agent through the payment method/s on the site is not a purchase of any product or service but a donation to the Canibuy cause.
 </p>
 <p>
+<strong>
 11. PROHIBITED TRANSACTIONS
+</strong>
 </p>
 <p>
 11.1 Disposal Agents agree that on receiving products from Canibuy, you will disposed the products using methods safe enough not to cause harm to another person or to the Disposal Agent.
@@ -114,7 +138,9 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 12.1 Disposal Agents understand that Canibuy will charge fees to them for the delivery of the products to be disposed of. These fees are available on the Site and may change from time to time. Your acceptance of this agreement indicates your acceptance of these fees and any change to these fees which may occur now or in the future.
 </p>
 <p>
+<strong>
 13. DISCLAIMER OF WARRANTIES
+</strong>
 </p>
 <p>
 13.1 YOU UNDERSTAND AND AGREE THAT THIS SITE AND THE INFORMATION, SERVICES, PRODUCTS AND MATERIALS AVAILABLE THROUGH IT ARE PROVIDED ON AN “AS IS” AND “AS AVAILABLE” BASIS. YOU EXPRESSLY AGREE THAT USE OF THIS SITE IS AT YOUR SOLE RISK.
@@ -138,38 +164,73 @@ BY AGREEING TO THIS AGREEMENT AND USING THE CANIBUY WEBSITE (THE “SITE”), YO
 13.7 ALL THE INFORMATION APPEARING ON THIS SITE IS PROVIDED WITHOUT A REPRESENTATION OR WARRANTY WHATSOEVER, WHETHER EXPRESSED OR IMPLIED, AND CANIBUY DISCLAIMS ANY LIABILITY TO THE DISPOSAL AGENT IN THIS REGARD.
 </p>
 <p>
+<strong>
 14. LIMITATION OF LIABILITY
+</strong>
 </p>
 <p>
-21.1 Canibuy has taken reasonable steps as far as is possible, to ensure the accuracy and completeness of the content shown on this site, to ensure that the Disposal Agent doesn’t suffer any loss or damages as a result of the use of this site.
-
-21.2 UNDER NO CIRCUMSTANCES, INCLUDING WITHOUT LIMITATION NEGLIGENCE, SHALL CANIBUY OR ITS PARENTS, SUBSIDIARIES, AFFILIATES, OFFICERS, DIRECTORS, EMPLOYEES, AGENTS, SUPPLIERS OR ANY OTHER PARTY INVOLVED IN CREATING, PRODUCING OR DELIVERING FOR CANIBUY BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR PUNITIVE DAMAGES ARISING FROM OR IN CONNECTION WITH THE USE OR INABILITY TO USE CANIBUY OR ANY CONTENT PROVIDED BY OR THROUGH THIS SITE, OR RESULTING FROM UNAUTHORIZED ACCESS TO OR ALTERATION OF YOUR TRANSMISSIONS OR DATA, OR OTHER INFORMATION THAT IS SENT OR RECEIVED, INCLUDING BUT NOT LIMITED TO DAMAGES FOR LOST PROFITS, USE, DATA OR OTHER INTANGIBLES, EVEN IF CANIBUY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-
-21.3 CANIBUY SHALL HAVE NO LIABILITY TO DISPOSAL AGENTS IN CONNECTION WITH ANY ACTIONS OR TRANSACTIONS BY AN INDIVIDUAL THAT USES DISPOSAL AGENT USER NAME AND PASSWORD WITH THEIR CONSENT OR KNOWLEDGE OR ANY CAUSE OVER WHICH CANIBUY DOES NOT HAVE DIRECT CONTROL, INCLUDING, BUT NOT LIMITED TO, PROBLEMS ATTRIBUTABLE TO COMPUTER HARDWARE OR SOFTWARE (INCLUDING COMPUTER VIRUSES), TELEPHONES (OR OTHER COMMUNICATIONS EQUIPMENT), OR INTERNET SERVICE PROVIDERS.
-
-21.4 CANIBUY SHALL HAVE NO LIABILITY TO DISPOSAL AGENTS OR THIRD PARTIES IN CONNECTION WITH ANY THE PRODUCTS THAT ARE TO BE DISPOSED OF BEING MISUSED OR MISHANDLED BY DISPOSAL AGENTS. IT IS EXPRESSLY UNDERSTOOD THAT THE DISPOSAL AGENTS IS AWARE THAT CANIBUY WEBSITE IS A DEMONSTRATIONAL WEBSITE AND IS USED TO SHOWCASE SOFTWARE DEVELOPMENT CAPABILITIES OF CANIBUY AND IS NOT AN ONLINE STORE THAT SELLS THE PRODUCTS DISPLAYED THERE.
-
-21.5 Canibuy shall not be liable for any fraud, deception or misrepresentations by Disposal Agents of the Site's services in any capacity whatsoever.
-
-21.6 In no event shall Canibuy be liable for any act or omission of any third party (such as, for example, any provider of telecommunications services, internet access or computer equipment or software) or for any circumstances beyond our control (such as, for example, a fire, flood, or other natural disaster, war, riot, strike, act of civil or military authority, equipment failure, computer virus, or failure or interruption of electrical, telecommunications or other utility services).
-
-21.7 Some jurisdictions do not allow the limitation or exclusion of liability for incidental or consequential damages, in such jurisdictions, liability is limited to the fullest extent permitted by law.
-
-22. DISPOSAL AGENT’S LIABILITY TO CANIBUY
-In the event that Canibuy takes action against you for breach of these terms of use, you agree to reimburse Canibuy for all legal costs, including all fees related to evidence gathering and collection commission, on a scale as between attorney and own client.
-
-23. APPLICABLE LAW
-The terms and conditions pertaining to any products or services appearing on this site or pages shall be governed and interpreted in accordance with the laws of the Republic of South Africa. Use of the service offered on this site or pages will constitute the Disposal Agents consent and submission to the jurisdiction of the South African courts regarding all proceedings, transactions, applications or the like instituted by either party against the other, arising from any of the terms and conditions pertaining to this site or such products or services.
-
-24. TERMINATION
-Notwithstanding any of these terms and conditions, Canibuy reserves the right, without notice and in its sole discretion, to terminate your license to use this Site, and to block or prevent future access to and use of this Site.
-
-25. SEVERABILITY
-If any provision of this Disposal Agent agreement is held to be invalid, void or for any reason unenforceable, then that provision shall be deemed severable from this Disposal Agent agreement and shall not affect the validity and enforceability of any remaining provisions.
-
-27. TERMS
-27.1 The terms “Disposal Agent”,“You” and “Agent” are used interchangeably and refer to all individuals and/or entities accessing this web site as Disposal Agents for any reason.
-
-27.2 The terms “we” and “Canibuy” are used interchangeably and refer to Canibuy itself and all individuals and/or entities acting directly on behalf of Canibuy.
+14.1 Canibuy has taken reasonable steps as far as is possible, to ensure the accuracy and completeness of the content shown on this site, to ensure that the Disposal Agent doesn’t suffer any loss or damages as a result of the use of this site.
+</p>
+<p>
+14.2 UNDER NO CIRCUMSTANCES, INCLUDING WITHOUT LIMITATION NEGLIGENCE, SHALL CANIBUY OR ITS PARENTS, SUBSIDIARIES, AFFILIATES, OFFICERS, DIRECTORS, EMPLOYEES, AGENTS, SUPPLIERS OR ANY OTHER PARTY INVOLVED IN CREATING, PRODUCING OR DELIVERING FOR CANIBUY BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR PUNITIVE DAMAGES ARISING FROM OR IN CONNECTION WITH THE USE OR INABILITY TO USE CANIBUY OR ANY CONTENT PROVIDED BY OR THROUGH THIS SITE, OR RESULTING FROM UNAUTHORIZED ACCESS TO OR ALTERATION OF YOUR TRANSMISSIONS OR DATA, OR OTHER INFORMATION THAT IS SENT OR RECEIVED, INCLUDING BUT NOT LIMITED TO DAMAGES FOR LOST PROFITS, USE, DATA OR OTHER INTANGIBLES, EVEN IF CANIBUY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+</p>
+<p>
+14.3 CANIBUY SHALL HAVE NO LIABILITY TO DISPOSAL AGENTS IN CONNECTION WITH ANY ACTIONS OR TRANSACTIONS BY AN INDIVIDUAL THAT USES DISPOSAL AGENT USER NAME AND PASSWORD WITH THEIR CONSENT OR KNOWLEDGE OR ANY CAUSE OVER WHICH CANIBUY DOES NOT HAVE DIRECT CONTROL, INCLUDING, BUT NOT LIMITED TO, PROBLEMS ATTRIBUTABLE TO COMPUTER HARDWARE OR SOFTWARE (INCLUDING COMPUTER VIRUSES), TELEPHONES (OR OTHER COMMUNICATIONS EQUIPMENT), OR INTERNET SERVICE PROVIDERS.
+</p>
+<p>
+14.4 CANIBUY SHALL HAVE NO LIABILITY TO DISPOSAL AGENTS OR THIRD PARTIES IN CONNECTION WITH ANY THE PRODUCTS THAT ARE TO BE DISPOSED OF BEING MISUSED OR MISHANDLED BY DISPOSAL AGENTS. IT IS EXPRESSLY UNDERSTOOD THAT THE DISPOSAL AGENTS IS AWARE THAT CANIBUY WEBSITE IS A DEMONSTRATIONAL WEBSITE AND IS USED TO SHOWCASE SOFTWARE DEVELOPMENT CAPABILITIES OF CANIBUY AND IS NOT AN ONLINE STORE THAT SELLS THE PRODUCTS DISPLAYED THERE.
+</p>
+<p>
+14.5 Canibuy shall not be liable for any fraud, deception or misrepresentations by Disposal Agents of the Site's services in any capacity whatsoever.
+</p>
+<p>
+14.6 In no event shall Canibuy be liable for any act or omission of any third party (such as, for example, any provider of telecommunications services, internet access or computer equipment or software) or for any circumstances beyond our control (such as, for example, a fire, flood, or other natural disaster, war, riot, strike, act of civil or military authority, equipment failure, computer virus, or failure or interruption of electrical, telecommunications or other utility services).
+</p>
+<p>
+14.7 Some jurisdictions do not allow the limitation or exclusion of liability for incidental or consequential damages, in such jurisdictions, liability is limited to the fullest extent permitted by law.
+</p>
+<p>
+<strong>
+15. DISPOSAL AGENT’S LIABILITY TO CANIBUY
+</strong>
+</p>
+<p>
+15.1 In the event that Canibuy takes action against you for breach of these terms of use, you agree to reimburse Canibuy for all legal costs, including all fees related to evidence gathering and collection commission, on a scale as between attorney and own client.
+</p>
+<p>
+<strong>
+16. APPLICABLE LAW
+</strong>
+</p>
+<p>
+16.1 The terms and conditions pertaining to any products or services appearing on this site or pages shall be governed and interpreted in accordance with the laws of the Republic of South Africa. Use of the service offered on this site or pages will constitute the Disposal Agents consent and submission to the jurisdiction of the South African courts regarding all proceedings, transactions, applications or the like instituted by either party against the other, arising from any of the terms and conditions pertaining to this site or such products or services.
+</p>
+<p>
+<strong>
+17. TERMINATION
+</strong>
+</p>
+<p>
+17.1 Notwithstanding any of these terms and conditions, Canibuy reserves the right, without notice and in its sole discretion, to terminate your license to use this Site, and to block or prevent future access to and use of this Site.
+</p>
+<p>
+<strong>
+18. SEVERABILITY
+</strong>
+</p>
+<p>
+18.1 If any provision of this Disposal Agent agreement is held to be invalid, void or for any reason unenforceable, then that provision shall be deemed severable from this Disposal Agent agreement and shall not affect the validity and enforceability of any remaining provisions.
+</p>
+<p>
+<strong>
+19. TERMS
+</strong>
+</p>
+<p>
+19.1 The terms “Disposal Agent”,“You” and “Agent” are used interchangeably and refer to all individuals and/or entities accessing this web site as Disposal Agents for any reason.
+</p>
+<p>
+19.2 The terms “we” and “Canibuy” are used interchangeably and refer to Canibuy itself and all individuals and/or entities acting directly on behalf of Canibuy.
+</p>
 </p>
 </html>
