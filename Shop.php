3ded8d7823113b1a7f9b4d3e8a0d5857f81d2608
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-27 11:07:06 +0200

    Update Shop.php

--- Shop.php
+++ Shop.php
@@ -771,7 +771,7 @@ close
 <div class="container">
 <div class="topbar-content">
 <div class="top-bar-left"> 
-<span> <?php echo $_GET['username']; ?></span><span> </span> <span>[<a href="Logout.php?&auth=<?php echo $_GET['auth'];?>"><span style="color:red;"> Log out</span></a>]</span><span>[<a href="T1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>"><span style="color:orange;"> Order Tracker</span></a>]</span>
+<span> <?php echo $_GET['username']; ?></span><span> </span> <span>[<a href="Logout.php?&auth=<?php echo $_GET['auth'];?>"><span style="color:red;"> Log out</span></a>]</span><span>[<a href="process1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>"><span style="color:orange;"> Order Tracker</span></a>]</span>
 </div>
 <div class="top-bar-right">
 <div class="topbar-menu">
