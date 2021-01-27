ca0442fafcd5b67f498fddf72a3567fbf0d4cc4c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-27 10:28:22 +0200

    Update processT.php

--- processT.php
+++ processT.php
@@ -2,16 +2,13 @@
 session_start();
 $user = $_SESSION['username'];
 $auth = $_SESSION['token'];
+echo $user;
+echo $auth;
 ?>
 <html>
 <form action="T1.php" method="get" id="key">
-<input name="username" type="hidden" value="<?php echo $user ?>">
-<input name="auth" type="hidden" value="<?php echo $auth ?>">
+<input name="username" type="hidden" value="<?php echo $user; ?>">
+<input name="auth" type="hidden" value="<?php echo $auth; ?>">
 </form>
-<script>
-function keys(){
-document.getElementById("key").submit();
-}
-keys();
-</script>
+
 <html>
