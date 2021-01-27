a44ff0b60a916b71b1d92499b3170d1255e1d010
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-27 03:35:11 +0200

    Update processf.php

--- processf.php
+++ processf.php
@@ -1,4 +1,8 @@
 <html>
+<form action="F1.php" method="get" id="key">
+<input name="username" type="hidden" value="" id="user">
+<input name="auth" type="hidden" value="" id="auth">
+</form>
 <script>
 function keys(){
 let username = localStorage.getItem('username'); 
