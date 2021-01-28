e21b09e676afb0ea06696e33b258cbdba56825a8
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-28 11:36:59 +0200

    Update process3.php

--- process3.php
+++ process3.php
@@ -4,50 +4,12 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$ot=GET_['auth'];
-$sql='DELETE FROM oders WHERE idOrderToken="'.$ot.'"';
+$username = $_GET['username'];
+$authtoken = $_GET['auth'];
+$sql='DELETE FROM oders WHERE idOrderToken="'.$authtoken.'"';
 mysqli_query($conn, $sql);
-$sql2='DELETE FROM address WHERE idToken="'.$ot.'"';
+$sql2='DELETE FROM address WHERE idToken="'.$authtoken.'"';
 mysqli_query($conn, $sql2);
-$sql3='DELETE FROM deliverytoken WHERE idOrderID="'.$ot.'"';
+$sql3='DELETE FROM deliverytoken WHERE idOrderID="'.$authtoken.'"';
 mysqli_query($conn, $sql3);
 ?>
-
-<html>
-<form action="process1.php" method="get" id="form2">
-<input type="hidden" name="auth" value="" id="auth2">
-</form>
-
-<form action="F1.php" method="get" id="form">
-<input type="hidden" name="username" value="" id="user">
-<input type="hidden" name="auth" value="" id="auth">
-</form>
-
-<script>
-function submit2(){
-let auth2 = localStorage.getItem('token');
-document.getElementById("auth2").value = auth2 ;
-document.getElementById("form2").submit();
-}
-submit2();
-</script>
-
-
-
-<script>
-function user(){
-let user = localStorage.getItem('username');
-document.getElementById("user").value = user;
-}
-function auth(){
-let auth = localStorage.getItem('token');
-document.getElementById("auth").value = auth ;
-}
-function submit(){
-document.getElementById("form").submit();
-}
-user();
-auth();
-submit();
-</script>
-</html>
