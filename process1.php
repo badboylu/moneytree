308fbe53b34606564958bfdd2416dacfe584ef36
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-28 10:18:37 +0200

    Update process1.php

--- process1.php
+++ process1.php
@@ -1,9 +1,22 @@
 <html>
+<form action="F1.php" method="get" id="form">
+<input type="hidden" name="auth" value="" id="auth">
+<input type="hidden" name="username" value="" id="user">
+</form>
 <script>
 function show(){
 let user = localStorage.getItem('username');
-document.getElementById("show").value = user;
+document.getElementById("user").value = user;
 }
-show()
+function show1(){
+let auth = localStorage.getItem('token');
+document.getElementById("auth").value = auth ;
+}
+function submit(){
+document.getElementById("form").submit();
+}
+show();
+show1();
+submit();
 </script>
 </html>
