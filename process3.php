f450f0b33113ddff5e5ad3a44f6c9a0fa002757b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-28 11:30:45 +0200

    Create process3.php

---
+++ process3.php
@@ -0,0 +1,53 @@
+<?php
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="1e8f896b7da9e41";
+$dBName="heroku_61db5a5cdc2dfd8";
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$ot=GET_['auth'];
+$sql='DELETE FROM oders WHERE idOrderToken="'.$ot.'"';
+mysqli_query($conn, $sql);
+$sql2='DELETE FROM address WHERE idToken="'.$ot.'"';
+mysqli_query($conn, $sql2);
+$sql3='DELETE FROM deliverytoken WHERE idOrderID="'.$ot.'"';
+mysqli_query($conn, $sql3);
+?>
+
+<html>
+<form action="process1.php" method="get" id="form2">
+<input type="hidden" name="auth" value="" id="auth2">
+</form>
+
+<form action="F1.php" method="get" id="form">
+<input type="hidden" name="username" value="" id="user">
+<input type="hidden" name="auth" value="" id="auth">
+</form>
+
+<script>
+function submit2(){
+let auth2 = localStorage.getItem('token');
+document.getElementById("auth2").value = auth2 ;
+document.getElementById("form2").submit();
+}
+submit2();
+</script>
+
+
+
+<script>
+function user(){
+let user = localStorage.getItem('username');
+document.getElementById("user").value = user;
+}
+function auth(){
+let auth = localStorage.getItem('token');
+document.getElementById("auth").value = auth ;
+}
+function submit(){
+document.getElementById("form").submit();
+}
+user();
+auth();
+submit();
+</script>
+</html>
