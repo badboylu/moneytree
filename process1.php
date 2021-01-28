a9a220275c7c3c1ded900393b5f2b285a9337e63
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-28 10:56:15 +0200

    Update process1.php

--- process1.php
+++ process1.php
@@ -1,3 +1,13 @@
+<?php
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="1e8f896b7da9e41";
+$dBName="heroku_61db5a5cdc2dfd8";
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$sql='DELETE FROM Oders WHERE idOrderToken="'.$ot.'"';
+mysqli_query($conn, $sql);
+    
+
 <html>
 <form action="F1.php" method="get" id="form">
 <input type="hidden" name="username" value="" id="user">
