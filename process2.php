74319b3821fb42769a32fb22f37ee601427ae867
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 07:30:58 +0200

    Update process2.php

--- process2.php
+++ process2.php
@@ -1,31 +1,24 @@
-<?php
- $servername="us-cdbr-east-02.cleardb.com";
- $dBUsername="b7fcd41c893d7a";
- $dBPassword="1e8f896b7da9e41";
- $dBName="heroku_61db5a5cdc2dfd8";
- $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-
-    $token= bin2hex(random_bytes(16));
-    $sql="INSERT INTO userauth2 (token) VALUES (?);";
-    $stmt= mysqli_stmt_init($conn);
-
-if(!mysqli_stmt_prepare($stmt, $sql)){
-    header("Location:Signin.php?error=sqlerror123");
-    exit();
- }
-
- mysqli_stmt_bind_param($stmt,"s",$token);
- mysqli_stmt_execute($stmt);
-?>
 <html>
+
+<form action="process4.php" method="get" id="form">
+<input type="hidden" name="username" value="" id="user">
+<input type="hidden" name="auth" value="" id="auth">
+</form>
+
 <script>
-function clear(){
-localStorage.clear();
+function user(){
+let user = localStorage.getItem('username');
+document.getElementById("user").value = user;
 }
-clear();
+function auth(){
+let auth = localStorage.getItem('token');
+document.getElementById("auth").value = auth ;
+}
+function submit(){
+document.getElementById("form").submit();
+}
+user();
+auth();
+submit();
 </script>
 </html>
-<?php
- header("Location:T1.php?auth=".$token);
- exit();
-?>
