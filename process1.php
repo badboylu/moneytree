fcc20d1906d50b4d0bb342ce87d9931c930f84ba
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 13:19:06 +0200

    Update process1.php

--- process1.php
+++ process1.php
@@ -17,49 +17,7 @@ document.getElementById("auth").value = auth ;
 function submit(){
 document.getElementById("form").submit();
 }
-function clear(){
 
-  localStorage.removeItem("BatchCC");
-  localStorage.removeItem("totalAmountCC");
-  localStorage.removeItem("totalCostCC");
-  localStorage.removeItem("cartNumberCC");
-
-  localStorage.removeItem("PacksBW");
-  localStorage.removeItem("totalAmountBW");
-  localStorage.removeItem("totalCostBW");
-  localStorage.removeItem("cartNumberBW");
-
-  localStorage.removeItem("GramsCCes");
-  localStorage.removeItem("totalAmountCCES");
-  localStorage.removeItem("totalCostCCes");
-  localStorage.removeItem("cartNumberCCES");
-
-  localStorage.removeItem("GramsDP");
-  localStorage.removeItem("totalAmountDP");
-  localStorage.removeItem("totalCostDP");
-  localStorage.removeItem("cartNumberDP");
-
-  localStorage.removeItem("GramsNL");
-  localStorage.removeItem("totalAmountNL");
-  localStorage.removeItem("totalCostNL");
-  localStorage.removeItem("cartNumberNL");
-
-  localStorage.removeItem("GramsOC");
-  localStorage.removeItem("totalAmountOC");
-  localStorage.removeItem("totalCostOC");
-  localStorage.removeItem("cartNumberOC");
-
-  localStorage.removeItem("GramsPE");
-  localStorage.removeItem("totalAmountPE");
-  localStorage.removeItem("totalCostPE");
-  localStorage.removeItem("cartNumberPE");
-
-  localStorage.removeItem("GramsPR");
-  localStorage.removeItem("totalAmountPR");
-  localStorage.removeItem("totalCostPR");
-  localStorage.removeItem("cartNumberPR");
-}
-clear();
 user();
 auth();
 submit();
