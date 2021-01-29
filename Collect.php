4dfb9a3285697019f8b16293761597bbbac3a0b0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 10:46:43 +0200

    Update Collect.php

--- Collect.php
+++ Collect.php
@@ -4,8 +4,9 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$authtoken=$_GET['auth'];
+$auth=$_GET['auth'];
 $user=$_GET['username'];
+$collect=$_GET['Collect'];
 $driver='Lunga';
 $stat='Collected';
 $sql='SELECT * FROM userauth2 WHERE token=?';
@@ -14,7 +15,7 @@ $sql='SELECT * FROM userauth2 WHERE token=?';
        header("Location:Signin.php?error=SQL1");
        exit();
 }
-   mysqli_stmt_bind_param($stmt,"s",$authtoken);
+   mysqli_stmt_bind_param($stmt,"s",$auth);
    mysqli_stmt_execute($stmt);
    mysqli_stmt_store_result($stmt);
    $count=mysqli_stmt_num_rows($stmt);
@@ -22,17 +23,6 @@ $sql='SELECT * FROM userauth2 WHERE token=?';
     header("Location:Signin.php?error=notloggedin");
     exit();
 }
-$sql='SELECT idOrderID FROM deliverytoken WHERE idPrepperToken="'.$authtoken.'" ';
-$result=mysqli_query($conn,$sql);
-
-while($row = mysqli_fetch_array($result)) {
- $token[]=$row['idOrderID'];
-  }
-
-foreach ($token as $key) {
-}
-
-echo $key;
 
 $new2='SELECT * FROM deliverytoken ';
 $result2=mysqli_query($conn,$new2);
@@ -42,21 +32,16 @@ while($row2 = mysqli_fetch_array($result2)) {
   }
 
 foreach ($token2 as $keys) {
-  if ($keys==$authtoken){
+  if ($keys==$collect){
       $sql="UPDATE deliverytoken SET idPrepperToken='".$stat."' WHERE idPrepperToken='".$authtoken."' ";
       mysqli_query($conn,$sql);
       $sql2="UPDATE address SET idOrderConfirmation='".$stat."' WHERE idToken='".$key."' ";
       mysqli_query($conn,$sql2);
       $sql3="UPDATE oders SET idOrderConfirmation='".$stat."' WHERE idOrderToken='".$key."' ";
       mysqli_query($conn,$sql3);
-      header("Location:DL77.php?username=".$user."&auth=".$authtoken."");
+      header("Location:DL77.php?username=".$user."&auth=".$auth."");
       exit();
       
-}else{
-      
-} 
-}
- 
 
 
 
