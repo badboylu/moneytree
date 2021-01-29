f2038bae814ede7b9a1dc008a9ef1ba8b6038516
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 07:49:56 +0200

    Update process4.php

--- process4.php
+++ process4.php
@@ -16,6 +16,16 @@ while($row=mysqli_fetch_array($result)){
 
 $ordertoken=Max($token);
 
+$sql="INSERT INTO oders (idOrderConfirmation) VALUES(?)";
+$stmt=mysqli_stmt_init($conn);
+
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:delivery.php?error=SQL6");
+       exit();
+}
+     mysqli_stmt_bind_param($stmt,"siiiiiiiissss",$username,$dp,$cc,$oc,$bw,$pr,$nl,$cces,$pe,$confirmedpayment1,$authtoken,$time,$date,$total);
+     mysqli_stmt_execute($stmt);
+     mysqli_stmt_close($stmt);
 
 ?>
 <html>
