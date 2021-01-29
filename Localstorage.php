44146dec0b6555c615a6f53184b3ed0426438ce5
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 12:35:38 +0200

    Update Localstorage.php

--- Localstorage.php
+++ Localstorage.php
@@ -1,25 +1,8 @@
-<?php
-$lunga='Magagula';
-?>
-<html>
-<span id="show">
-Test
-</span>
+
 <script>
-
 function test(){
-let test = '<?php echo $lunga; ?>';
-if(test){
   localStorage.setItem('username', test);
- }
 }
-
-function show(){
-let name = localStorage.getItem('username');
-document.getElementById("show").textContent = name;
-}
-
-show();
 test();
 </script>
 </html>
