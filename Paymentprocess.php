8d88cbe27bf8f6961825469ae6286dadae6bae3e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 08:02:49 +0200

    Update Paymentprocess.php

--- Paymentprocess.php
+++ Paymentprocess.php
@@ -102,7 +102,7 @@ $data = array(
 $signature = generateSignature($data);
 $data['signature'] = $signature;
 
-$htmlForm = '<form action="https://www.payfast.co.za/eng/process" method="post" id="form">';
+$htmlForm = '<form action="https://sandbox.payfast.co.za​/eng/process" method="post" id="form">';
 foreach($data as $name=> $value)
 {
     $htmlForm .= '<input name="'.$name.'" type="hidden" value="'.$value.'" />';
