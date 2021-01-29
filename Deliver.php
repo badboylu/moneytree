53e8c30e94a54d1cd636fdad4032eb219a74732f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-29 12:13:17 +0200

    Update Deliver.php

--- Deliver.php
+++ Deliver.php
@@ -4,21 +4,19 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$authtoken = $_GET['Deliver'];
+$custicode = $_GET['Deliver'];
+$auth = $_GET['auth'];
 $driver='Lunga';
 $stat='Delivered';
 
-$sql='SELECT idOrderID FROM deliverytoken WHERE idCustomerToken="'.$authtoken.'" ';
-$result=mysqli_query($conn,$sql);
+$query='SELECT * FROM deliverytoken WHERE idCustomerToken="'.$custicode.'" ';
+$result=mysqli_query($conn,$query);
 
-while($row = mysqli_fetch_array($result)) {
- $token[]=$row['idOrderID'];
-  }
-
-foreach ($token as $key) {
+while($row=mysqli_fetch_array($result)){
+ $ordernumber[]=$row['idOrderID'];
 }
 
-echo $key;
+$authcode=Min($ordernumber);
 
 $new2='SELECT * FROM deliverytoken ';
 $result2=mysqli_query($conn,$new2);
@@ -28,14 +26,14 @@ while($row2 = mysqli_fetch_array($result2)) {
   }
 
 foreach ($token2 as $keys) {
-  if ($keys==$authtoken){
-      $sql3="UPDATE deliverytoken SET idPrepperToken='".$stat."' WHERE idCustomerToken='".$authtoken."' ";
+  if ($keys==$custicode){
+      $sql3="UPDATE deliverytoken SET idPrepperToken='".$stat."' WHERE idCustomerToken='".$custicode."' ";
       mysqli_query($conn,$sql3);
-      $sql="UPDATE deliverytoken SET idCustomerToken='".$driver."' WHERE idCustomerToken='".$authtoken."' ";
+      $sql="UPDATE deliverytoken SET idCustomerToken='".$driver."' WHERE idCustomerToken='".$custicode."' ";
       mysqli_query($conn,$sql);
-      $sql2="UPDATE address SET idOrderConfirmation='".$stat."' WHERE idToken='".$key."' ";
+      $sql2="UPDATE address SET idOrderConfirmation='".$stat."' WHERE idToken='".$authcode."' ";
       mysqli_query($conn,$sql2);
-      $sql4="UPDATE oders SET idOrderConfirmation='".$stat."' WHERE idOrderToken='".$key."' ";
+      $sql4="UPDATE oders SET idOrderConfirmation='".$stat."' WHERE idOrderToken='".$authcode."' ";
       mysqli_query($conn,$sql4);
       header("Location:DL01.php");
       exit();
