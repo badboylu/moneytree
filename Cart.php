a914bc4b596d995a8b8b8e86043b25545d5aea0e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-30 00:38:21 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -648,8 +648,8 @@ class="single-product-page single-product-content product-design-default product
                 <span class="totalDelivery">R40</span>
                 <br>
                 <span class="totalCart"><strong>Promotional discount</strong></span>
-                <span class="totalOrderR1">%</span>
-                <span class="totalOrder1"></span>
+                <span>%</span>
+                <span>40</span>
                 <br>
                 <span class="totalCart"><strong>Items</strong></span>
                 <span class="totalOrderR">R</span>
