107b13797161e83873d4b1cb920a5a3d787ceab4
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-30 14:20:12 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -822,11 +822,18 @@ discount();
 </script>
 
 <script>
+
 setInterval (function setTotal(){
+let check = localStorage.getItem('discount');
+if(!check){
 let total = localStorage.getItem('totalAmountNumber'); 
 total = parseInt(total) + 40;
 document.getElementById("total").value = total;
 }, 1000);
+}else{
+let discounttotal = localStorage.getItem('discount'); 
+document.getElementById("total").value =  discounttotal;
+}
 setTotal();
 </script>
 
