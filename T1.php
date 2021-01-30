78b4da26c0294036336efd06ecb7064c8eb55a8a
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-30 23:51:40 +0200

    Update T1.php

--- T1.php
+++ T1.php
@@ -16,6 +16,18 @@
    }
    $order=Min($ordernumbers);   
 
+   $sql="SELECT * FROM oder WHERE idOrderConfirmation=? AND idOrderUsername='".$username."' AND idOrders='".$order."' ";
+   $stmt=mysqli_stmt_init($conn);
+   mysqli_stmt_bind_param($stmt,"s",$prepared);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $prep=mysqli_stmt_num_rows($stmt);
+   if(!$prep){
+    $prep = '0';
+   }
+  
+   
+
 ?>
 <!DOCTYPE html> <!--[if IE 8]><html class="ie ie8" lang="en-US"> <![endif]--> <!--[if !(IE 7) & !(IE 8)]><!--><html lang="en-US"> <!--<![endif]-->
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
