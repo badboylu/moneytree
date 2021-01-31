749062e1226372dd66831d13a9f3322a97d90f21
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-01-31 16:11:16 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -3,13 +3,13 @@
 <script>
 function sendemail(){
 Email.send({
-    Host : "smtp.yourisp.com",
-    Username : "username",
-    Password : "password",
-    To : 'them@website.com',
-    From : "you@isp.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
+    Host : "mail.canibuy.co.za port 58",
+    Username : "no-reply@canibuy.co.za",
+    Password : "God@loves",
+    To : 'liyandza.magagula@gmail.com',
+    From : "no-reply@canibuy.co.za",
+    Subject : "Send with php by Lunga Magagula",
+    Body : "I am proud to type that you have sent this email with code written by yourself boi!"
 }).then(
   message => alert(message)
 );
