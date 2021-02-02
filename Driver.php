07cf6d7e59a8904b26830fb0cd77395462518084
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-02 13:34:42 +0200

    Update Driver.php

--- Driver.php
+++ Driver.php
@@ -338,7 +338,43 @@ if(!$count2){
 <!--script admin-->
 <script src="admin-script.js"></script>
 
-<script src="hideAddress.js"></script>
+<script>
+   function hideOrder1(){
+   var mallhide = '<?php print_r($row['idCrib']); ?>';
+   if(mallhide == 'Mall'){
+   document.getElementById("hideBuilding").style.display = "none";
+   document.getElementById("hideEstate").style.display = "none";
+   document.getElementById("hideComplex").style.display = "none";
+   document.getElementById("hideHouse").style.display = "none";
+   document.getElementById("hideUnit").style.display = "none";
+   document.getElementById("hideStreet").style.display = "none";
+   }
+   if(mallhide == 'Shop'){
+   document.getElementById("hideBuilding").style.display = "none";
+   document.getElementById("hideEstate").style.display = "none";
+   document.getElementById("hideComplex").style.display = "none";
+   document.getElementById("hideHouse").style.display = "none";
+   document.getElementById("hideUnit").style.display = "none";
+   document.getElementById("hideMall").style.display = "none";
+   }
+   if(mallhide == 'House'){
+   document.getElementById("hideBuilding").style.display = "none";
+   document.getElementById("hideEstate").style.display = "none";
+   document.getElementById("hideComplex").style.display = "none";
+   document.getElementById("hideHouse").style.display = "none";
+   document.getElementById("hideUnit").style.display = "none";
+   document.getElementById("hideMall").style.display = "none";
+   }
+   if(mallhide == 'Estate'){
+   document.getElementById("hideBuilding").style.display = "none";
+   document.getElementById("hideComplex").style.display = "none";
+   document.getElementById("hideHouse").style.display = "none";
+   document.getElementById("hideUnit").style.display = "none";
+   document.getElementById("hideMall").style.display = "none";
+   }
+}
+hideOrder1();
+</script>
 
 </body>
 
