5d60f2143f551bd23b54d0c7af4e91cbf2fd5a16
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-04 09:01:30 +0200

    Update Ordersystem.php

--- Ordersystem.php
+++ Ordersystem.php
@@ -43,7 +43,7 @@ $custitoken=Min($token);
 
 
       if($custitoken){
-      $sql="UPDATE oder SET idOrderConfirmation='".$orderready."' WHERE idOrderCustiCode='58efe8' ";
+      $sql="UPDATE oder SET idOrderConfirmation='".$orderready."' WHERE idOrderUsername='Lunga' ";
       mysqli_query($conn,$sql);
       $sql2="UPDATE deliverytoken SET idUser='".$user."' WHERE idOrderCustomerToken='".$custitoken."'  ";
       mysqli_query($conn,$sql2);
