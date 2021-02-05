60f002f606cbacc37019f510e80a0516bf2059fd
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-05 09:56:16 +0200

    Update O1.php

--- O1.php
+++ O1.php
@@ -15,6 +15,24 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Font Awesome-->
+    <link rel="stylesheet" type="text/css" href="fontawesome.css">
+
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="flag-icon.css">
+
+    <!-- Datatable css-->
+    <link rel="stylesheet" type="text/css" href="datatables.css">
+
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
+
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="admin.css">
 <link rel="profile" 
 href="http://gmpg.org/xfn/11">
 <link rel="pingback" 
