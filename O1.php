87fd9c24c9fb7bf857414cb4bc0c954a530b1836
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-05 09:43:04 +0200

    Update O1.php

--- O1.php
+++ O1.php
@@ -181,14 +181,14 @@ Log
                             <tr>
                             <?php 
                             while($rows=mysqli_fetch_assoc($result)){
-                            while($rows=mysqli_fetch_assoc($result2)){
+                            
                             ?>
                                 <td>#00<?php echo $rows['idOrder']; ?></td>
                                 <td><?php echo $rows['idDate']; ?></td>
                                 <td>R<?php echo $rows['idOrderTotal']; ?></td>
                                 <td><?php echo $rows['idOrderConfirmation']; ?></td>
                             
-                            <?php }} ?>
+                            <?php } ?>
                             </tr>
                             </tbody>
                         </table>
