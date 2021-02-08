145fe58e4cce9c8cb7d8dbb2fa069c65718328f0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-08 09:20:12 +0200

    Update DabcapsES.php

--- DabcapsES.php
+++ DabcapsES.php
@@ -436,12 +436,10 @@ Edibles
 <i class="fa fa-copyright">
 </i> 
 2020 Created by 
-<span style="color: orange;font-size: 15px">
-Plug
+<span>
+Kahrent Technology
 </span>
-Tech Creations
-</a>
-. Premium e-commerce solutions.</div></div></div></div></footer></div>
+Africa. Contact us <a href="C.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">(here)</a>. Terms and conditions <a href="A1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">(here)</a></div></div></div></div></footer></div>
 <div class="basel-close-side">
 </div>
 <div class="basel-toolbar icons-design-line basel-toolbar-label-show">
