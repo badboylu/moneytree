8e80ef35c4aa285d80f3e5ae3937e9bd5e10e7b5
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-13 02:15:11 +0200

    Update process3.php

--- process3.php
+++ process3.php
@@ -6,32 +6,23 @@ $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $username = $_GET['username'];
 $authtoken = $_GET['auth'];
-
 $query='SELECT * FROM oders WHERE idOrderToken="'.$authtoken.'" ';
 $result=mysqli_query($conn,$query);
-
 while($row=mysqli_fetch_array($result)){
  $token[]=$row['idOrders'];
 }
-
 $ordertoken=Max($token);
-
 $query1='SELECT * FROM address WHERE idToken="'.$authtoken.'" ';
 $result1=mysqli_query($conn,$query1);
-
 while($row1=mysqli_fetch_array($result1)){
  $token1[]=$row1['idUser'];
 }
-
 $ordertoken1=Max($token1);
-
 $query2='SELECT * FROM deliverytoken WHERE idOrderID="'.$authtoken.'" ';
 $result2=mysqli_query($conn,$query2);
-
 while($row2=mysqli_fetch_array($result2)){
  $token2[]=$row2['idOrder'];
 }
-
 $ordertoken2=Max($token2);
 
 $sql='DELETE FROM oders WHERE idOrderToken="'.$authtoken.'" AND idOrders="'.$ordertoken.'"';
@@ -39,7 +30,9 @@ mysqli_query($conn, $sql);
 $sql2='DELETE FROM address WHERE idToken="'.$authtoken.'" AND idUser="'.$ordertoken1.'"';
 mysqli_query($conn, $sql2);
 $sql3='DELETE FROM deliverytoken WHERE idOrderID="'.$authtoken.'" AND idOrder="'.$ordertoken2.'"';
-mysqli_query($conn, $sql3);    
+mysqli_query($conn, $sql3);
+$sql4='DELETE FROM orderlog WHERE idOrderNumber="'.$ordertoken2.'" ;
+mysqli_query($conn, $sql4);    
 ?>
 <html>
 <form action="F1.php" method="get" id="form">
