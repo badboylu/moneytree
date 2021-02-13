a7e7a1e67a8be29a91ae32facb6fbceb2b5b22ac
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-13 03:23:51 +0200

    Update Deliver.php

--- Deliver.php
+++ Deliver.php
@@ -9,7 +9,6 @@ $auth = $_GET['auth'];
 $driver = $_GET['username'];
 $stat='Delivered';
 $username = $_GET['username'];
-
    $sql='SELECT * FROM userauth2 WHERE token=?';
    $stmt=mysqli_stmt_init($conn);
  if(!mysqli_stmt_prepare($stmt,$sql)){
@@ -24,23 +23,23 @@ $username = $_GET['username'];
     header("Location:Signin.php?error=notloggedin");
     exit();
 }
-
 $query='SELECT * FROM deliverytoken WHERE idCustomerToken="'.$custicode.'" ';
 $result=mysqli_query($conn,$query);
-
 while($row=mysqli_fetch_array($result)){
  $ordernumber[]=$row['idOrderID'];
 }
-
 $authcode=Min($ordernumber);
-
+$query2='SELECT * FROM deliverytoken WHERE idCustomerToken="'.$custicode.'" ';
+$result2=mysqli_query($conn,$query2);
+while($row=mysqli_fetch_array($result2)){
+ $ordernumber2[]=$row2['idOrders'];
+}
+$ordernumber=Max($ordernumber2);
 $new2='SELECT * FROM deliverytoken ';
 $result2=mysqli_query($conn,$new2);
-
 while($row2 = mysqli_fetch_array($result2)) {
  $token2[]=$row2["idCustomerToken"];
   }
-
 foreach ($token2 as $keys) {
   if ($keys==$custicode){
       $sql3="UPDATE deliverytoken SET idPrepperToken='".$stat."' WHERE idCustomerToken='".$custicode."' ";
