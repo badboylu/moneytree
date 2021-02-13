86ec61c71c90c0131cbbb09b628fc8f01fdfe5db
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-13 03:11:13 +0200

    Update process4.php

--- process4.php
+++ process4.php
@@ -6,19 +6,22 @@ $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $username = $_GET['username'];
 $authtoken = $_GET['auth'];
-
 $query='SELECT * FROM oders WHERE idOrderToken="'.$authtoken.'" ';
 $result=mysqli_query($conn,$query);
-
 while($row=mysqli_fetch_array($result)){
  $token[]=$row['idOrders'];
 }
-
 $ordertoken=Max($token);
-
+$query2='SELECT * FROM deliverytoken WHERE idOrderID="'.$authtoken.'" ';
+$result2=mysqli_query($conn,$query2);
+while($row2=mysqli_fetch_array($result2)){
+ $token2[]=$row2['idOrder'];
+}
+$ordertoken2=Max($token2);
 $sql="UPDATE oders SET idOrderConfirmation='Pending' WHERE idOrderToken='".$authtoken."' AND idOrders='".$ordertoken."' ";
 mysqli_query($conn,$sql);
-
+$sql2="UPDATE orderlog SET idOrderStat='Pending' WHERE idOrderNumber='".$ordertoken2."' ";
+mysqli_query($conn,$sql2);
 ?>
 
 <html>
