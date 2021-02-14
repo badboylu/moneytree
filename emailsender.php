9e30f9fceba91ee3f6e34602b434687af4061d59
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-14 09:56:58 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -3,12 +3,12 @@
 <script>
 function sendemail(){
 Email.send({
-    Host : "mail.canibuy.co.za",
-    Username : "no-reply@canibuy.co.za",
+    Host : "mail.canibuy.site",
+    Username : "no-reply@canibuy.site",
     Password : "God@loves",
     To : 'liyandza.magagula@gmail.com',
     From : "no-reply@canibuy.co.za",
-    Subject : "Send with php by Lunga Magagula",
+    Subject : "Sent with php by Lunga Magagula",
     Body : "I am proud to type that you have sent this email with code written by yourself boi!"
 }).then(
   message => alert(message)
@@ -17,3 +17,4 @@ Email.send({
 sendemail();
 </script> 
 </html>
+
