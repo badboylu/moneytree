2ca79fb39dd0bfb649b48e792814a9144a2978fb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 19:55:04 +0200

    Update FPW.php

--- FPW.php
+++ FPW.php
@@ -10,21 +10,6 @@
    date_default_timezone_set('Africa/Johannesburg');
    $userEmail=$_GET["email"];
  
-  $sql="SELECT * FROM users WHERE emailUsers=?";
-  $stmt=mysqli_stmt_init($conn);
-  if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:Reset.php?error=SQL1");
-       exit();
- }
-   mysqli_stmt_bind_param($stmt,"s",$userEmail);
-   mysqli_stmt_execute($stmt);
-   mysqli_stmt_store_result($stmt);
-   $count=mysqli_stmt_num_rows($stmt);
-   mysqli_stmt_close($stmt);
- if (!$count>0){
-    header("Location:Reset.php?error=emailnotfound");
-    exit();
- }
 
 $sql="INSERT INTO pwdrest (pwdResetEmail,pwdResetToken,pwdResetDate) VALUES (?,?,?);";
     $stmt= mysqli_stmt_init($conn);
