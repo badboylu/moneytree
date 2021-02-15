12afa161ddc0de5f3af95a7dd36f955388a13763
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 20:47:59 +0200

    Update R1.php

--- R1.php
+++ R1.php
@@ -65,7 +65,7 @@
 <script>
 function sendemail(){
 let count="<?php echo $count ?>";
-if(count!>0){
+
 Email.send({
     Host : "mail.canibuy.site",
     Username : "no-reply@canibuy.site",
@@ -75,7 +75,7 @@ Email.send({
     Subject : "Reset Canibuy password",
     Body : '<p>Hello,</p><p>we have recieved a request to reset your account password on the canibuy site. We have generated a link for you to reset your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us. Thank you</p>'              
 });
-}
+
 }
 sendemail();
 </script> 
