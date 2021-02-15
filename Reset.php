bf5b3f34da8fb42d04490c35cdb49348f054cddb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 12:27:04 +0200

    Update Reset.php

--- Reset.php
+++ Reset.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kushlord password reset</title>
+    <title>Password reset</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="iofrm-style.css">
