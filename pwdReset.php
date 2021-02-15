54b87464664f5988d5cf3becf4184e3965dc8789
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 21:46:00 +0200

    Update pwdReset.php

--- pwdReset.php
+++ pwdReset.php
@@ -1,1 +1,29 @@
+<?php
+
+$servername="us-cdbr-east-02.cleardb.com";
+$dBUsername="b7fcd41c893d7a";
+$dBPassword="1e8f896b7da9e41";
+$dBName="heroku_61db5a5cdc2dfd8";
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$auth=$_POST['auth'];
+$email=$_POST['email'];
+$password=$_POST['pwd'];
+$passwordrepeat=$_POST['pwdrepeat'];
+
+   if($_POST['pwd'] != $_POST['pwdrepeat']){
+       header("Location:R2.php?error=pwdnomatch"."&username=".$username."&email=".$email);
+       exit();
+   }
+
+     $uppercase = preg_match('@[A-Z]@', $password);
+     $lowercase = preg_match('@[a-z]@', $password);
+     $number    = preg_match('@[0-9]@', $password);
+     $specialChars = preg_match('@[^\w]@', $password);
+
+   if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+       header("Location:R2.php?error=invalidpwd"."&auth=".$auth);
+       exit();
+}    
+?>
+
 
