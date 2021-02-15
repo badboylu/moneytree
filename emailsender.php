3914c55afb881e5dfe70b5875f2fdcbcba7ff14e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 08:54:33 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -11,15 +11,7 @@ Email.send({
     To : 'liyandza.magagula@gmail.com',
     From : "no-reply@canibuy.site",
     Subject : "Reset Canibuy password",
-    Body : "
-    Hello, 
-    we recieved a request from you to reset your account password on the canibuy.co.za site.
-    We have generated a link for you to reset your password below. Please click on the link to change your password.
-    
-    Password reset link: www.canibuy.co.za/FPW.php?selector="<?php echo $auth ?>"
-    
-    If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us.
-    "
+    Body : "Is it working? "
 }).then(
   message => alert(message)
 );
