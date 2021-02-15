0b9d0529b2240801275b3fe0549a3f354551003e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 17:58:58 +0200

    Update R1.php

--- R1.php
+++ R1.php
@@ -1,25 +1,42 @@
 <?php
+   
    $servername="us-cdbr-east-02.cleardb.com";
    $dBUsername="b7fcd41c893d7a";
    $dBPassword="1e8f896b7da9e41";
    $dBName="heroku_61db5a5cdc2dfd8";
    $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-   $authtoken = $_GET['auth'];
-   $sql='SELECT * FROM pwdrest WHERE pwdResetToken=?';
-   $stmt=mysqli_stmt_init($conn);
- if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:Signin.php?error=SQL1");
+   $auth=bin2hex(random_bytes(8));
+   $date = date("Y-m-d");
+   date_default_timezone_set('Africa/Johannesburg');
+   $userEmail=$_GET["email"];
+ 
+  $sql="SELECT * FROM users WHERE emailUsers=?";
+  $stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Reset.php?error=SQL1");
        exit();
-}
-   mysqli_stmt_bind_param($stmt,"s",$authtoken);
+ }
+   mysqli_stmt_bind_param($stmt,"s",$userEmail);
    mysqli_stmt_execute($stmt);
    mysqli_stmt_store_result($stmt);
    $count=mysqli_stmt_num_rows($stmt);
+   mysqli_stmt_close($stmt);
  if (!$count>0){
-    header("Location:Signin.php?error=notloggedin");
+    header("Location:Reset.php?error=emailnotfound");
     exit();
-}
- 
+ }
+
+$sql="INSERT INTO pwdrest (pwdResetEmail,pwdResetToken,pwdResetDate) VALUES (?,?,?);";
+    $stmt= mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql)){
+       header("Location:Reset.php?error=SQL1");
+       exit();
+ }
+
+ mysqli_stmt_bind_param($stmt,"sss",$userEmail,$auth,$date);
+ mysqli_stmt_execute($stmt);
+
 ?>
 <html lang="en">
 
