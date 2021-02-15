475234ee9b238bd19f48e82683d0a7a80789ed1a
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 20:20:00 +0200

    Update R1.php

--- R1.php
+++ R1.php
@@ -1,5 +1,4 @@
 <?php
-   
    $servername="us-cdbr-east-02.cleardb.com";
    $dBUsername="b7fcd41c893d7a";
    $dBPassword="1e8f896b7da9e41";
@@ -8,7 +7,6 @@
    $auth=$_GET['auth'];
 ?>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
