fdd4914594cbc9e0ecd4e166f8a6b5706d7364bf
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 16:26:27 +0200

    Update FPW.php

--- FPW.php
+++ FPW.php
@@ -8,7 +8,6 @@
    $auth=bin2hex(random_bytes(8));
    $date = date("Y-m-d");
    date_default_timezone_set('Africa/Johannesburg');
-
    $userEmail=$_GET["email"];
  
   $sql="SELECT * FROM users WHERE emailUsers=?";
@@ -46,10 +45,10 @@ if(!mysqli_stmt_prepare($stmt, $sql)){
 function sendemail(){
 Email.send({
     Host : "mail.canibuy.site",
-    Username : "support@canibuy.site",
+    Username : "no-reply@canibuy.site",
     Password : "God@loves",
     To : 'liyandza.magagula@gmail.com',
-    From : "support@canibuy.site",
+    From : "no-reply@canibuy.site",
     Subject : "Reset Canibuy password",
     Body : '<p>Hello,</p><p>we have recieved a request to reset your account password on the canibuy site. We have generated a link for you to reset your password below. Please click on the link below to change your password.</p><p><a href="www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us. Thank you</p>'              
 }).then(
