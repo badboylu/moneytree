3cefc5e37b0abd06a954f6db3961c00ffba9d855
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-15 12:51:42 +0200

    Update FPW.php

--- FPW.php
+++ FPW.php
@@ -14,7 +14,7 @@
   if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:register.html?error=SQL1");
        exit();
-}
+ }
    mysqli_stmt_bind_param($stmt,"s",$userEmail);
    mysqli_stmt_execute($stmt);
    mysqli_stmt_store_result($stmt);
