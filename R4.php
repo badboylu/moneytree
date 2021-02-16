0cf1ead863b349e22b4cfa9d13056e204ce0c78e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 16:01:26 +0200

    Update R4.php

--- R4.php
+++ R4.php
@@ -65,8 +65,8 @@ $password=$_GET['auth'];
 </script>
 <script>
 function sendemail(){
-let count=<?php echo $count ?>;
-if(!count>0){
+let count="<?php echo $count ?>";
+if(count=="0"){
 Email.send({
     Host : "mail.canibuy.site",
     Username : "no-reply@canibuy.site",
@@ -82,6 +82,7 @@ sendemail();
 </script> 
 </html>
 <?php
+if($count=0){
 $sql="INSERT INTO pwdrest (pwdResetEmail) VALUES (?);";
     $stmt= mysqli_stmt_init($conn);
 if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -90,5 +91,5 @@ if(!mysqli_stmt_prepare($stmt, $sql)){
  }
  mysqli_stmt_bind_param($stmt,"s",$email);
  mysqli_stmt_execute($stmt);
- echo $count;
+ }
 ?>
