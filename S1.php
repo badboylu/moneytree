6df91fdd9851ce2831df9a2c9764350f780c9f4b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 10:49:34 +0200

    Update S1.php

--- S1.php
+++ S1.php
@@ -1,4 +1,10 @@
 <?php
+   $servername="us-cdbr-east-02.cleardb.com";
+   $dBUsername="b7fcd41c893d7a";
+   $dBPassword="1e8f896b7da9e41";
+   $dBName="heroku_61db5a5cdc2dfd8";
+   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+
 $username=$_GET['username'];
 $email=$_GET['email'];
 $password=$_GET['auth'];
