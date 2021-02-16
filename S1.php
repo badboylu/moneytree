d1c8dba433ba9d36e7c6309a3b17fbf347705bf2
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 11:56:48 +0200

    Update S1.php

--- S1.php
+++ S1.php
@@ -9,6 +9,22 @@ $username=$_GET['username'];
 $email=$_GET['email'];
 $password=$_GET['auth'];
 
+  $sql="SELECT * FROM pwdrest WHERE pwdResetEmail=?";
+  $stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Reset.php?error=SQL1");
+       exit();
+ }
+   mysqli_stmt_bind_param($stmt,"s",$email);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $count=mysqli_stmt_num_rows($stmt);
+   mysqli_stmt_close($stmt);
+ if (!$count>0){
+    header("Location:Signin.php?error=notloggedin");
+    exit();
+ }
+
     $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
     $stmt=mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt,$sql)){
