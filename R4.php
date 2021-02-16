52b4fd1cc59dc166abe4bfd96ac101eb5ed1d6e1
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 09:09:22 +0200

    Create R4.php

---
+++ R4.php
@@ -0,0 +1,55 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account registration</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="iofrm-theme17.css">
+</head>
+<body>
+    <div class="form-body without-side">
+        <div class="website-logo">
+            <a href="index.html">
+                <div class="logo">
+                    <img class="logo-size" src="images/logo-light.svg" alt="">
+                </div>
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+                    <img src="images/graphic3.svg" alt="">
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3><span style="color:green"> Account activation link sent!</span></h3>
+                        
+                        <p>Check your email for the link to activate your account</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://smtpjs.com/v3/smtp.js">
+</script>
+<script>
+function sendemail(){
+Email.send({
+    Host : "mail.canibuy.site",
+    Username : "no-reply@canibuy.site",
+    Password : "God@loves",
+    To : 'liyandza.magagula@gmail.com',
+    From : "no-reply@canibuy.site",
+    Subject : "Canibuy account activation",
+    Body : '<p>Hello,</p><p>We at Canibuy welcome you to our store. The last step to getting your account active is to confirm your email address. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us. Thank you</p>'              
+  });
+}
+sendemail();
+</script> 
+</html>
