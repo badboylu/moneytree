ff18d9d6b548d6e51e218ab914581e6190eaa071
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 17:58:31 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -13,8 +13,8 @@ Email.send({
     Password : "God@loves",
     To : '<?php echo $email ?>',
     From : "no-reply@canibuy.site",
-    Subject : "Reset Canibuy password",
-    Body : '<p>Hello,</p><p>we have recieved a request to reset your account password on the canibuy site. We have generated a link for you to reset your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us. Thank you</p>'              
+    Subject : "Canibuy account activation",
+    Body : '<p>Hello <?php echo username; ?>,</p><p><span style="color:orange">Canibuy</span> welcomes you to the online store. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/S1.php?signup=complete&username=<?php echo $username; ?>&email=<?php echo $email; ?>&auth=<?php echo $password; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send an email to support@canibuy.site.</p><p>Thank you</p>'              
 });
 }
 sendemail();
