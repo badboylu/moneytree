95b07951cc79a8af1309d6a872a0ed54585461fc
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 18:16:01 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -15,7 +15,7 @@ Email.send({
     To : '<?php echo $email ?>',
     From : "no-reply@canibuy.site",
     Subject : "Canibuy account activation",
-    Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">Canibuy</span> welcomes you to the online store. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/S1.php?signup=complete&username=<?php echo $username; ?>&email=<?php echo $email; ?>&auth=<?php echo $password; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send an email to support@canibuy.site.</p><p>Thank you</p>'              
+    Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">canibuy</span> welcomes you to the <span style="color:green">online store</span>. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/S1.php?signup=complete&username=<?php echo $username; ?>&email=<?php echo $email; ?>&auth=<?php echo $password; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send an email to support@canibuy.site.</p><p>Thank you</p>'              
 }).then(
   message => alert(message)
 );
