f814bc779b41e2bc5f181555e9bff4c7c06cb964
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 10:05:36 +0200

    Update S1.php

--- S1.php
+++ S1.php
@@ -1,13 +1,15 @@
 <?php
+$username=$_GET['username'];
+$email=$_GET['email'];
+$password=$_GET['auth'];
 
     $sql="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES(?,?,?)";
     $stmt=mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:register.php?error=SQL3");
        exit();
-    }
-     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);
+}
+     $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
+     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$password);
      mysqli_stmt_execute($stmt);
      mysqli_stmt_store_result($stmt);
-       header("Location:Shop.php?signup=successful");
-       exit();
