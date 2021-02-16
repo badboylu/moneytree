09f78ddc73a20811d0a668a96bef09ce330c0503
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 21:01:48 +0200

    Update R4.php

--- R4.php
+++ R4.php
@@ -71,10 +71,10 @@ Email.send({
     Host : "mail.canibuy.site",
     Username : "no-reply@canibuy.site",
     Password : "God@loves",
-    To : '<?php echo $email ?>',
+    To : '<?php echo $email; ?>',
     From : "no-reply@canibuy.site",
     Subject : "Canibuy account activation",
-    Body : '<p>Hello <?php echo username; ?>,</p><p><span style="color:orange">Canibuy</span> welcomes you to the online store. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/S1.php?signup=complete&username=<?php echo $username; ?>&email=<?php echo $email; ?>&auth=<?php echo $password; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send an email to support@canibuy.site.</p><p>Thank you</p>'              
+    Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">canibuy</span> welcomes you to the <span style="color:green">online store</span>. Please click on the link below to activate your account.</p><p><a href="https://www.canibuy.co.za/S1.php?signup=complete&username=<?php echo $username; ?>&email=<?php echo $email; ?>&auth=<?php echo $password; ?>"> Account activation link </a></p><p>If you did not make this request or need assistance, please send an email to support@canibuy.site.</p><p>Thank you</p>'              
   });
  }
 }
