845d4a98afba2ea3b5163f3ff243c40ebdb1583e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-16 08:46:38 +0200

    Update Signin.php

--- Signin.php
+++ Signin.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
+?>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -39,9 +40,10 @@
                             <input class="form-control" type="password" name="pwd" placeholder="Password" required>
                             <p id="password" style="font-size:14px; color:red; display:none;">*incorrect password</p>
                             <p id="notloggedin" style="font-size:14px; color:red; display:none;">*Please login</p>
-                            <p id="successful" style="font-size:14px; color:red; display:none;">*Logged out</p>
+                            <p id="successful" style="font-size:14px; color:orange; display:none;">*Logged out</p>
                             <p id="successful2" style="font-size:14px; color:green; display:none;">Thank you for visiting <span style="color:orange;">canibuy.co.za</span>. We hope you come again soon!</p>
                             <p id="successful3" style="font-size:14px; color:green; display:none;">Sign-up to Canibuy successful. Welcome! Please log in</p>
+                            <p id="successful4" style="font-size:14px; color:orange; display:none;">Password successfully reset</p>
                             <div class="form-button">
                                 <button type="submit" class="ibtn" >Login</button> 
                                <a href="Reset.php">Forgot password?</a>
