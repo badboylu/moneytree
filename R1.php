e6cb089d3eefae898b870bab17f55604727086a6
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-17 11:14:43 +0200

    Update R1.php

--- R1.php
+++ R1.php
@@ -35,13 +35,7 @@
 </head>
 <body>
     <div class="form-body without-side">
-        <div class="website-logo">
-            <a href="index.html">
-                <div class="logo">
-                    <img class="logo-size" src="images/logo-light.svg" alt="">
-                </div>
-            </a>
-        </div>
+        
         <div class="row">
             <div class="img-holder">
                 <div class="bg"></div>
