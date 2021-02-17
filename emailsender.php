22788c87701d9450358fff1856069ac279e8c57f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-18 01:30:33 +0200

    Update emailsender.php

--- emailsender.php
+++ emailsender.php
@@ -13,7 +13,7 @@ Email.send({
     Username : "no-reply@canibuy.site",
     Password : "God@loves",
     To : '<?php echo $email; ?>',
-    From : "no-reply@canibuy.site",
+    From : "Canibuy Online Health Store",
     Subject : "Reset Canibuy password",
     Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">canibuy</span> recieved a request to reset your account password. A link has been generated for you to <span style="color:green">reset</span> your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us.</p><p>Thank you</p>'              
           
