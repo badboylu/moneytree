7def475fdcceb9bba3ff8f6e0bd6eef1408c7833
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-17 07:10:33 +0200

    Update Signin.php

--- Signin.php
+++ Signin.php
@@ -40,7 +40,7 @@ $signup=$_GET['signup'];
                             
                             <input class="form-control" type="password" name="pwd" placeholder="Password" required>
                             <p id="password" style="font-size:14px; color:red; display:none;">*incorrect password</p>
-                            <p id="notloggedin" style="font-size:14px; color:red; display:none;">*Please login</p>
+                            <p id="notloggedin" style="font-size:14px; color:red; display:none;">*Not logged in</p>
                             <p id="successful" style="font-size:14px; color:orange; display:none;">Logged out</p>
                             <p id="successful2" style="font-size:14px; color:green; display:none;">Thank you for visiting</p>
                             <p id="successful3" style="font-size:14px; color:green; display:none;">Sign-up to Canibuy successful. Welcome! Please log in</p>
