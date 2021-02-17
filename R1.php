8c9b29a7c9a89b1f04d848a41874f6d7f18221ac
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-17 08:06:13 +0200

    Update R1.php

--- R1.php
+++ R1.php
@@ -72,7 +72,7 @@ Email.send({
     To : '<?php echo $email; ?>',
     From : "no-reply@canibuy.site",
     Subject : "Reset Canibuy password",
-    Body : '<p>Hello,</p><p>we have recieved a request to reset your account password on the <span style="color:orange">canibuy</span> site. We have generated a link for you to <span style="color:green">reset</span> your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us. Thank you</p>'              
+    Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">canibuy</span> recieved a request to reset your account password. A link has been generated for you to <span style="color:green">reset</span> your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us.</p><p>Thank you</p>'              
   });
  }
 }
