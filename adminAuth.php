a52f989c7ce4f8083c9a6627e331d98118986d0d
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-17 21:23:01 +0200

    Update adminAuth.php

--- adminAuth.php
+++ adminAuth.php
@@ -163,6 +163,10 @@ $result=mysqli_query($conn,$query);
                             <?php } ?>
                             </tbody>
                         </table>
+                                <br>
+                                <form action="AuthReset.php" method="post" id="form">
+                                <input type="submit" name="Next" value="Clear records" id="Blanks" "/>
+                                </form>
                     </div>
                 </div>
             </div>
