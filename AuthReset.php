1ee661b5aed70c730f565d682e32f2b6d7e528d0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-17 21:28:02 +0200

    Update AuthReset.php

--- AuthReset.php
+++ AuthReset.php
@@ -4,7 +4,7 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$sql="DELETE FROM user2";
+$sql="DELETE FROM userauth2";
 mysqli_query($conn, $sql);
     header("Location:adminAuth.php");
     exit();
