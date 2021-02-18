4616d0d679d6db9d9fe738d5a15021c5773c7087
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-18 19:52:47 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,9 @@
-<?php
-$total='250';
-$city='Pretoria';
-$town='Pretoria  East';
-?>
 <html>
 <script>
 function set(){
-localStorage.setItem('eTotal', <?php echo $total; ?>);
-localStorage.setItem('eCity', <?php echo $city; ?>);
-localStorage.setItem('eTown', <?php echo $town; ?>);
+localStorage.setItem('eTotal', 250);
+localStorage.setItem('eCity', Pretoria East);
+localStorage.setItem('eTown', Moreleta park;
 }
 set();
 </script>
