41c0857e0c31771b1bc4d8b5f1298ce41a7b030d
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-19 18:03:31 +0200

    Update T1.php

--- T1.php
+++ T1.php
@@ -403,8 +403,8 @@ Email.send({
     Password : "God@loves",
     To : '<?php echo $email; ?>',
     From : "no-reply@canibuy.site",
-    Subject : "",
-    Body : '<p>Hello <?php echo $username; ?>,</p><p><span style="color:orange">canibuy</span> recieved a request to reset your account password. A link has been generated for you to <span style="color:green">reset</span> your password below. Please click on the link below to change your password.</p><p><a href="https://www.canibuy.co.za/R2.php?auth=<?php echo $auth; ?>"> Password reset link </a></p><p>If you did not make this request or need assistance, please send a email to support@canibuy.site and inform us.</p><p>Thank you</p>'              
+    Subject : "Canibuy transaction details",
+    Body : '<p>Hello <?php echo $username; ?>,</p><p>you have successfully <span style="color:green">placed</span> an order at <span style="color:orange">canibuy</span>.</p><p><h1>Transaction details:</h1></p><p>Amount transacted:<?php echo $total; ?></p>'              
   });
  }
 }
