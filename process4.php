baf7227dd4a390e2e38794f1efd7be5d5aab47bc
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-19 09:26:06 +0200

    Update process4.php

--- process4.php
+++ process4.php
@@ -30,31 +30,55 @@ mysqli_query($conn,$sql2);
 <input type="hidden" name="auth" value="" id="auth">
 <input type="hidden" name="eTotal" value="" id="eTotal">
 <input type="hidden" name="eCity" value="" id="eCity">
+<input type="hidden" name="eTown" value="" id="eTown">
+<input type="hidden" name="eStreet" value="" id="eStreet">
+<input type="hidden" name="eSuburb" value="" id="eSuburb">
+<input type="hidden" name="eMall" value="" id="eMall">
+<input type="hidden" name="eBuilding" value="" id="eBuilding">
+<input type="hidden" name="eShop" value="" id="eShop">
+<input type="hidden" name="eComplex" value="" id="eComplex">
+<input type="hidden" name="eCompany" value="" id="eCompany">
+<input type="hidden" name="eHouseNumber" value="" id="eHouseNumber">
+<input type="hidden" name="eUnitNumber" value="" id="eUnitNumber">
+<input type="hidden" name="eEstate" value="" id="eEstate">
+<input type="hidden" name="eDP" value="" id="eDP">
+<input type="hidden" name="eCC" value="" id="eCC">
+<input type="hidden" name="eOC" value="" id="eOC">
+<input type="hidden" name="eBW" value="" id="eBW">
+<input type="hidden" name="eNL" value="" id="eNL">
+<input type="hidden" name="ePR" value="" id="ePR">
+<input type="hidden" name="eCCES" value="" id="eCCES">
+<input type="hidden" name="ePE" value="" id="ePE">
+<input type="hidden" name="eContact" value="" id="eContact">
+<input type="hidden" name="eDate" value="" id="eDate">
 </form>
 <script>
- 
 function user(){
 let user = localStorage.getItem('username');
 document.getElementById("user").value = user;
 }
- 
 function auth(){
 let auth = localStorage.getItem('token');
 document.getElementById("auth").value = auth ;
 }
- 
 function submit(){
 document.getElementById("form").submit();
 }
- 
 function total(){
 let total = localStorage.getItem('eTotal');
 document.getElementById("eTotal").value = total ;
 }
-
 function city(){
 let city = localStorage.getItem('eCity');
 document.getElementById("eCity").value = city ;
+}
+function town(){
+let town = localStorage.getItem('eTown');
+document.getElementById("eTown").value = town ;
+}
+function street(){
+let street = localStorage.getItem('eStreet');
+document.getElementById("eStreet").value = street ;
 }
 
 user();
