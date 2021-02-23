b57c3abbfc597c31ce7ca5ff586120b492908d80
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-23 16:02:58 +0200

    Update Scanner.php

--- Scanner.php
+++ Scanner.php
@@ -1,12 +1,31 @@
 <html>
-  <head>
-    
-    <title>Instascan</title>
-    <script type="text/javascript" src="instascan.min.js"></script>
-  </head>
-  <body>
-    <video id="preview" style="width:500px; height:500px"></video>
-    <script type="text/javascript">
+<head>
+<meta charset="utf-8">
+<meta content="stuff, to, help, search, engines, not" name="keywords">
+<meta content="What this page is about." name="description">
+<meta content="Display Webcam Stream" name="title">
+<title>Display Webcam Stream</title>
+<style>
+#container {
+    margin: 0px auto;
+    width: 500px;
+    height: 375px;
+    border: 10px #333 solid;
+}
+#videoElement {
+    width: 500px;
+    height: 375px;
+    background-color: #666;
+}
+</style>
+</head>
+<body>
+<div id="container">
+    <video autoplay="true" id="preview">
+    </video>
+</div>
+</body>
+<script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
         console.log(content);
@@ -21,5 +40,4 @@
         console.error(e);
       });
     </script>
-  </body>
 </html>
