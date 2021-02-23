5ebcaf9e972c5bf9401ed43d5bf8b1489bf625db
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-23 15:53:11 +0200

    Update Scanner.php

--- Scanner.php
+++ Scanner.php
@@ -5,7 +5,7 @@
     <script type="text/javascript" src="instascan.min.js"></script>
   </head>
   <body>
-    <video id="preview" style="width:500px; height:500px; margin:0px auto;"></video>
+    <video id="preview" style="width:500px; height:500px"></video>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
@@ -13,7 +13,7 @@
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+          scanner.start(cameras[1]);
         } else {
           console.error('No cameras found.');
         }
