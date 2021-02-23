bbd5eaa5dff9866a0c9c08ba5874b8fbe2212d82
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-23 15:37:45 +0200

    Update Scanner.php

--- Scanner.php
+++ Scanner.php
@@ -1,17 +1,11 @@
 <html>
   <head>
-    <style>
-   #preview{
-   width:500px;
-   height: 500px;
-   margin:0px auto;
-   }
-   </style>
+    
     <title>Instascan</title>
     <script type="text/javascript" src="instascan.min.js"></script>
   </head>
   <body>
-    <video id="preview"></video>
+    <video id="preview" style="width:500px; height:500px; margin:0px auto;"></video>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
