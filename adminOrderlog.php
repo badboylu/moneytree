1f05c5b99ef09e3b553a14fb05d48f10f1535b0e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-24 08:46:53 +0200

    Update adminOrderlog.php

--- adminOrderlog.php
+++ adminOrderlog.php
@@ -170,7 +170,7 @@ $result=mysqli_query($conn,$query);
                             </tbody>
                         </table>
                                 <br>
-                                <form action="OrderReset.php" method="post" id="form">
+                                <form action="OrderlogReset.php" method="post" id="form">
                                 <input type="submit" name="Next" value="Clear records" id="Blanks" "/>
                                 </form>
                     </div>
