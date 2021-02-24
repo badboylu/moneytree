78ccc23adee623beb369a84a02634b04a11bc260
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-24 16:32:36 +0200

    Update AdminCBI.php

--- AdminCBI.php
+++ AdminCBI.php
@@ -5,20 +5,7 @@
    $dBName="heroku_61db5a5cdc2dfd8";
    $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
    $authtoken = $_GET['auth'];
-   $sql='SELECT * FROM userauth2 WHERE token=?';
-   $stmt=mysqli_stmt_init($conn);
- if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:Signin.php?error=SQL1");
-       exit();
-}
-   mysqli_stmt_bind_param($stmt,"s",$authtoken);
-   mysqli_stmt_execute($stmt);
-   mysqli_stmt_store_result($stmt);
-   $count=mysqli_stmt_num_rows($stmt);
- if (!$count>0){
-    header("Location:Signin.php?error=notloggedin");
-    exit();
-}
+   
 ?>
 <!DOCTYPE html>
 <html lang="en">
