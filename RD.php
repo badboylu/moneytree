c11266f436706e238be9dbf308881afd366c6976
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-24 21:03:18 +0200

    Update RD.php

--- RD.php
+++ RD.php
@@ -25,13 +25,36 @@ $auth=$_GET['error'];
                 <div class="form-content">
                     <div class="form-items">
                         <h3>Distro account activation</h3>
-                        <p>Enter Distro's email address.</p>
+                        <p>Distro recruitment.</p>
                         <form action="FPW.php" method="get">
-                            <input class="form-control" type="text" name="email" placeholder="E-mail Address" required>
-                            <div class="form-button full-width">
-                                <button type="submit" class="ibtn">Send Activation Link</button>
-                            </div>
-                        </form>
+<label>Province:</label>
+<select name="Province" id="province" form="plug" required>
+  <option value="" disabled selected>Select Province</option>
+  <option value="Gauteng" >Gauteng</option>
+</select>
+   <label>City:</label>
+<select name="City" id="city" form="plug" required>
+  <option value="" disabled selected>Select City</option>
+  <option value="Johannesburg" >Johannesburg</option>
+  <option value="Pretoria" >Pretoria</option>
+</select>
+<label>Allocated area:</label>
+<select class="PTAtown" name="Towns" id="townsPTA" form="plug" required>
+  <option value="" disabled selected>Select Town</option>
+  <option value="PretoriaEast">Pretoria East</option>
+  <option value="PretoriaNorthEast" disabled >Pretoria North East</option>
+  <option value="PretoriaNorthWest" disabled >Pretoria North West</option>
+  <option value="PretoriaCentral" disabled >Pretoria Central</option>
+  <option value="PretoriaCentralWest" disabled >Pretoria Central West</option>
+  <option value="Centurion" disabled >Centurion</option>
+  <option value="Null" style="display:none" >Null</option>
+</select>
+<label>Distro's e-mail address:</label>
+<input class="form-control" type="text" name="email" placeholder="E-mail Address" required>
+<div class="form-button full-width">
+<button type="submit" class="ibtn">Send Activation Link</button>
+</div>
+</form>
                         
                    </div>
                 </div>
