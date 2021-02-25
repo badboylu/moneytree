335185f82392f2b15946d951ec81f9d6c3f80882
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-25 09:27:26 +0200

    Update RD3.php

--- RD3.php
+++ RD3.php
@@ -4,8 +4,11 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$username=$_POST['uid'];
+$province=$_POST['uid'];
+$city=$_POST['pwd'];
+$area=$_POST['pwd'];
 $email=$_POST['email'];
+$username=$_POST['pwd'];
 $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
 $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
@@ -13,7 +16,7 @@ $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
 $sql="SELECT uidUsers FROM users WHERE uidUsers=?";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.php?error=SQL1");
+       header("Location:RD2.php?error=SQL1");
        exit();
 }
 mysqli_stmt_bind_param($stmt,"s",$username);
@@ -21,17 +24,17 @@ mysqli_stmt_bind_param($stmt,"s",$username);
    mysqli_stmt_store_result($stmt);
    $resultCheck=mysqli_stmt_num_rows($stmt);
    if($resultCheck > 0){
-       header("Location:register.php?error=usernameexists");
+       header("Location:RD2.php?error=usernameexists");
        exit();
 } 
 $sql="SELECT emailUsers FROM users WHERE emailUsers=?";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:register.php?error=SQL2");
+       header("Location:RD2.php?error=SQL2");
        exit();
 }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       header("Location:register.php?error=invalidemail"."&username=".$username);
+       header("Location:RD2.php?error=invalidemail"."&username=".$username);
        exit();
 }
    mysqli_stmt_bind_param($stmt,"s",$email);
@@ -39,11 +42,11 @@ $stmt=mysqli_stmt_init($conn);
    mysqli_stmt_store_result($stmt);
    $resultCheck=mysqli_stmt_num_rows($stmt);
    if($resultCheck > 0){
-       header("Location:register.php?error=emailexists"."&username=".$username);
+       header("Location:RD2.php?error=emailexists"."&username=".$username);
        exit();
 } 
    if($_POST['pwd'] != $_POST['pwdrepeat']){
-       header("Location:register.php?error=pwdnomatch"."&username=".$username."&email=".$email);
+       header("Location:RD2.php?error=pwdnomatch"."&username=".$username."&email=".$email);
        exit();
 }
      $uppercase = preg_match('@[A-Z]@', $password);
@@ -51,9 +54,9 @@ $stmt=mysqli_stmt_init($conn);
      $number    = preg_match('@[0-9]@', $password);
      $specialChars = preg_match('@[^\w]@', $password);
    if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
-       header("Location:register.php?error=invalidpwd"."&username=".$username."&email=".$email);
+       header("Location:RD2.php?error=invalidpwd"."&username=".$username."&email=".$email);
        exit();
 }
-     header("Location:R4.php?signup=complete"."&username=".$username."&email=".$email."&auth=".$hashedPwd);
+     header("Location:Signin.php?distrosignup=complete"."&username=".$username."&email=".$email."&auth=".$hashedPwd);
      exit();
 ?>
