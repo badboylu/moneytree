6273b445153cb0101daccc653d5f2d4e1f8cd8bd
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-26 11:23:02 +0200

    Update RD3.php

--- RD3.php
+++ RD3.php
@@ -63,7 +63,7 @@ $stmt=mysqli_stmt_init($conn);
        header("Location:RD2.php?error=SQL3");
        exit();
 }
-     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$password);
+     mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);
      mysqli_stmt_execute($stmt);
      $sql="DELETE FROM pwdrest WHERE pwdResetEmail='".$email."' ";
      mysqli_query($conn,$sql);
