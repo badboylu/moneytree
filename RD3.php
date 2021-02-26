79cdf8e2b2c0d985b1a500c56fb12e5a13266d49
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-26 11:06:49 +0200

    Update RD3.php

--- RD3.php
+++ RD3.php
@@ -4,11 +4,11 @@ $dBUsername="b7fcd41c893d7a";
 $dBPassword="1e8f896b7da9e41";
 $dBName="heroku_61db5a5cdc2dfd8";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$province=$_POST['uid'];
-$city=$_POST['pwd'];
-$area=$_POST['pwd'];
+$province=$_POST['province'];
+$city=$_POST['city'];
+$area=$_POST['area'];
 $email=$_POST['email'];
-$username='Distro-'.$_POST['pwd'];
+$username='Distro-'.$_POST['username'];
 $password=$_POST['pwd'];
 $passwordrepeat=$_POST['pwdrepeat'];
 $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
@@ -57,16 +57,14 @@ $stmt=mysqli_stmt_init($conn);
        header("Location:RD2.php?error=invalidpwd"."&username=".$username."&email=".$email);
        exit();
 }
-     $sql="INSERT INTO distrouser (idDistro, idEmail, idPassword) VALUES(?,?,?)";
+    $sql="INSERT INTO distrouser (idDistro, idEmail, idPassword) VALUES(?,?,?)";
     $stmt=mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location:RD2.php?error=SQL3");
        exit();
 }
-     $hashedPwd=password_hash($password,PASSWORD_DEFAULT);
      mysqli_stmt_bind_param($stmt,"sss",$username,$email,$password);
      mysqli_stmt_execute($stmt);
-     mysqli_stmt_store_result($stmt);
      $sql="DELETE FROM pwdrest WHERE pwdResetEmail='".$email."' ";
      mysqli_query($conn,$sql);
      header("Location:Signin.php?signup=successful");
