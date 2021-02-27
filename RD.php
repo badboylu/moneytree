84db4a29f77b24fff79c0d9c541b69709a0d205b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-27 14:38:43 +0200

    Update RD.php

--- RD.php
+++ RD.php
@@ -27,30 +27,29 @@ $auth=$_GET['error'];
                         <h3>Distro account activation</h3>
                         <p>Distro recruitment</p>
 <form action="RD1.php" method="get">
-<div style"padding-bottom:4px"><label>Province:</label>
+<label>Province:</label>
 <select name="Province" id="province" form="plug" required>
-  <option value="" disabled selected>Select Province</option>
-  <option value="Gauteng" >Gauteng</option>
-</select></div>
-<div style"padding-bottom:4px"><label>City:</label>
+<option value="" disabled selected>Select Province</option>
+<option value="Gauteng" >Gauteng</option>
+</select>
+<label>City:</label>
 <select name="City" id="city" form="plug" required>
-  <option value="" disabled selected>Select City</option>
-  <option value="Johannesburg" >Johannesburg</option>
-  <option value="Pretoria" >Pretoria</option>
-</select></div>
-<div style"padding-bottom:4px"><label>Allocated area:</label>
+<option value="" disabled selected>Select City</option>
+<option value="Johannesburg" >Johannesburg</option>
+<option value="Pretoria" >Pretoria</option>
+</select>
+<label>Allocated area:</label>
 <select class="PTAtown" name="area" id="area" form="plug" required>
-  <option value="" disabled selected>Select Area</option>
-  <option value="PretoriaEast">Pretoria East</option>
-  <option value="PretoriaNorthEast" disabled >Pretoria North East</option>
-  <option value="PretoriaNorthWest" disabled >Pretoria North West</option>
-  <option value="PretoriaCentral" disabled >Pretoria Central</option>
-  <option value="PretoriaCentralWest" disabled >Pretoria Central West</option>
-  <option value="Centurion" disabled >Centurion</option>
-  <option value="Null" style="display:none" >Null</option>
-</select></div>
-<div><label>Distro's e-mail address:</label></div>
-<div><input class="form-control" type="text" name="email" placeholder="E-mail Address" required></div>
+<option value="" disabled selected>Select Area</option>
+<option value="PretoriaEast">Pretoria East</option>
+<option value="PretoriaNorthEast" disabled >Pretoria North East</option>
+<option value="PretoriaNorthWest" disabled >Pretoria North West</option>
+<option value="PretoriaCentral" disabled >Pretoria Central</option>
+<option value="PretoriaCentralWest" disabled >Pretoria Central West</option>
+<option value="Centurion" disabled >Centurion</option>
+</select>
+<label>Distro's e-mail address:</label>
+<input class="form-control" type="text" name="email" placeholder="E-mail Address" required>
 <div class="form-button full-width">
 <button type="submit" class="ibtn">Send Activation Link</button>
 </div>
