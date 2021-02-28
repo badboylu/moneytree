4346179f57b0a72c183a77396f574d2a43d9fdc6
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-28 19:44:37 +0200

    Update PrReset.php

--- PrReset.php
+++ PrReset.php
@@ -1,8 +1,8 @@
 <?php
-$servername="us-cdbr-east-02.cleardb.com";
-$dBUsername="b7fcd41c893d7a";
-$dBPassword="1e8f896b7da9e41";
-$dBName="heroku_61db5a5cdc2dfd8";
+$servername="dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dBUsername="fn5xp8yu9vmphif8";
+$dBPassword="j0oarzobzrwqy40b";
+$dBName="eebooc1cx2mejqru";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $sql="DELETE FROM pwdrest";
 mysqli_query($conn, $sql);
