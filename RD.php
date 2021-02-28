38f0fa353e1c24bc77347049d21b0cd96e9eed3f
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-28 08:01:53 +0200

    Update RD.php

--- RD.php
+++ RD.php
@@ -5,7 +5,7 @@ $auth=$_GET['error'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password reset</title>
+    <title>Recruitment</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="iofrm-style.css">
