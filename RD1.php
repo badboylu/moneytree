fcbba1e9fef43da18ddb16d308d036af716fd6c9
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-02-28 08:59:34 +0200

    Update RD1.php

--- RD1.php
+++ RD1.php
@@ -28,7 +28,7 @@ $email=$_GET['email'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Distro recruitment</title>
+    <title>Recruitment</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="iofrm-style.css">
