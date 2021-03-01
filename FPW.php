25687bf43a0688d7717a9f11be0a97d1d52059ee
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-01 13:46:21 +0200

    Update FPW.php

--- FPW.php
+++ FPW.php
@@ -8,12 +8,26 @@ $dBName="eebooc1cx2mejqru";
    $auth=bin2hex(random_bytes(8));
    $userEmail=$_GET["email"];
  
-  
+$sql="SELECT * FROM users WHERE emailUser=?";
+  $stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Reset.php?error=SQL2");
+       exit();
+ }
+   mysqli_stmt_bind_param($stmt,"s",$userEmail);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $count=mysqli_stmt_num_rows($stmt);
+   mysqli_stmt_close($stmt);
+ if (!$count>0){
+    header("Location:Reset.php?error=emailnotfound");
+    exit();
+ }  
 
 $sql="SELECT * FROM pwdrest WHERE pwdResetEmail=?";
   $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:Reset.php?error=SQL2");
+       header("Location:Reset.php?error=SQL3");
        exit();
  }
    mysqli_stmt_bind_param($stmt,"s",$email);
