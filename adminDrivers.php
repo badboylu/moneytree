33a7ed2aefb2fabc3f803a67dc33eb3bbb4afe42
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-03 22:03:38 +0200

    Update adminDrivers.php

--- adminDrivers.php
+++ adminDrivers.php
@@ -4,7 +4,7 @@ $dBUsername="fn5xp8yu9vmphif8";
 $dBPassword="j0oarzobzrwqy40b";
 $dBName="eebooc1cx2mejqru";
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-$query="select * from driverusers";
+$query="select * from driveruser";
 $result=mysqli_query($conn,$query);
 ?>
 <html lang="en">
