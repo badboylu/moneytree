adcc88df2952692200d355f846b157bcaa272b46
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-04 00:01:02 +0200

    Update and rename AdminDistro.php to Distro.php

--- Distro.php
+++ Distro.php
@@ -24,7 +24,7 @@
     <meta name="author" content="pixelstrap">
     <link rel="icon" href="favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
-    <title>Admin</title>
+    <title>Distribution Agent</title>
 
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
