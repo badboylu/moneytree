dd20e117ce60428c31b31a5b1dbec75b093bcb65
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-03 21:36:57 +0200

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -135,6 +135,52 @@ if(!mysqli_stmt_prepare($stmt, $sql)){
 }
 }   
 
+$sql="SELECT * FROM driverouser WHERE idDriver=? ";
+$stmt=mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Signin.html?error=D1");
+       exit();
+}
+   mysqli_stmt_bind_param($stmt,"s",$uid);
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_store_result($stmt);
+   $count=mysqli_stmt_num_rows($stmt);
+   mysqli_stmt_close($stmt);
+if ($count>0){
+   $sql='SELECT idPassword FROM driveruser WHERE idDriver="'.$uid.'"';
+   $stmt=mysqli_stmt_init($conn);
+ if(!mysqli_stmt_prepare($stmt,$sql)){
+       header("Location:Signin.php?error=SQL2");
+       exit();
+}
+   mysqli_stmt_execute($stmt);
+   mysqli_stmt_bind_result($stmt, $hash1);
+  
+   while (mysqli_stmt_fetch($stmt)) { 
+    if (password_verify($password, $hash1)) {
+    $token= bin2hex(random_bytes(16));
+    mysqli_stmt_close($stmt);
+    $sql="INSERT INTO userauth2 (username,token) VALUES (?,?);";
+    $stmt= mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql)){
+    header("Location:Signin.php?error=sqlerror123");
+    exit();
+ }
+
+ mysqli_stmt_bind_param($stmt,"ss",$uid,$token);
+ mysqli_stmt_execute($stmt);
+
+    header("Location:DH.php?login=successful"."&auth=".$token."&username=".$uid);
+    exit();
+
+} else {
+    header("Location:Signin.php?error=wrngpwd"."&username=".$uid);
+    exit();
+}
+}
+}   
+
 $sql="SELECT * FROM users WHERE emailUser=? OR uidUsers=? ";
 $stmt=mysqli_stmt_init($conn);
   if(!mysqli_stmt_prepare($stmt,$sql)){
