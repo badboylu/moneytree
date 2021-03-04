f2c5e9bbd67974cd19ce8205d4c941141cf850b2
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-04 15:51:28 +0200

    Update Driver.php

--- Driver.php
+++ Driver.php
@@ -22,7 +22,7 @@ $sql='SELECT * FROM userauth2 WHERE token=?';
     header("Location:Signin.php?error=notloggedin");
     exit();
 }
-$query='SELECT * FROM address WHERE idCustiToken="'.$custi.'" ';
+$query='SELECT * FROM address ';
 $result=mysqli_query($conn,$query);
 $row=mysqli_fetch_array($result);
 
