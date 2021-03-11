405a6d44c97e1e00b32c15fabbf445149d68bb2b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-11 08:17:44 +0200

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -38,25 +38,4 @@ $token= bin2hex(random_bytes(16));
         </div>
     </div>
 </body>
-<script 
-src="ShowHandlers.js" >
-</script>
-<script>
-function handler(){
- let confirmation="<?php echo $reset; ?>";
- if(confirmation=="successful"){
-  document.getElementById("successful4").style.display = "block";
- }
-}
-handler();
-</script>
-<script>
-function handler2(){
- let signup="<?php echo $signup; ?>";
- if(signup=="successful"){
-  document.getElementById("successful5").style.display = "block";
- }
-}
-handler2();
-</script>
 </html>
