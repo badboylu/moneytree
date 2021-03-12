964de0bf6b165885ea253db950f72f651d071102
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-12 17:29:18 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -131,7 +131,7 @@ href="cannacaps.html" />
 	<div class="wpb_column vc_column_container vc_col-sm-2">
 	<div class="vc_column-inner"><div class="wpb_wrapper">
 	<div class="wpb_text_column wpb_content_element vc_custom_1487879330860" >
-	<div class="wpb_wrapper">
+	 <div class="wpb_wrapper">
             <h5 style="text-align: center;">
             <a style="color: #1b1919; text-transform: uppercase; letter-spacing: 0.3px;" href="Strain 1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">
             <strong>
@@ -574,7 +574,7 @@ class="single-product-page single-product-content product-design-default product
                         </div>
                         <li> 
                         <img src="
-                        https://res.cloudinary.com/badboylu/image/upload/c_scale,h_300,w_235/v1612773186/IMG_20210204_140203_317_hprh8d.jpg"  class = "DPcontainer1">
+                        https://res.cloudinary.com/badboylu/image/upload/c_scale,h_300,w_235/v1595088889/_DSC6536_aeu5je.jpg"  class = "DPcontainer1">
                     </li>
                 <li>	
                 <span class = "DPcontainer2PE"><strong>Pineapple Express</strong></span>
