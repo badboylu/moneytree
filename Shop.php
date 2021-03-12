6334dfb6c232f8af7f1e03b58745103a561e9f38
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-12 17:14:33 +0200

    Update Shop.php

--- Shop.php
+++ Shop.php
@@ -1496,18 +1496,18 @@ New
 <img 
 width="273" 
 height="348" 
-src="https://res.cloudinary.com/badboylu/image/upload/c_scale,h_348,w_273/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg" 
+src="https://res.cloudinary.com/badboylu/image/upload/c_scale,h_348,w_273/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg" 
 class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" 
 alt="" 
 srcset=
-"https://res.cloudinary.com/badboylu/image/upload/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 870w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_300,w_235/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 235w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_980,w_768/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 768w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_1024,w_803/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 803w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_340,w_266/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 266w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_280,w_219/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 219w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_336,w_263/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 263w, 
-https://res.cloudinary.com/badboylu/image/upload/c_scale,h_671,w_526/v1596008969/b8d0de7cacaf88b8b43d5413142c274d_asslru.jpg 526w" 
+"https://res.cloudinary.com/badboylu/image/upload/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 870w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_300,w_235/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 235w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_980,w_768/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 768w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_1024,w_803/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 803w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_340,w_266/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 266w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_280,w_219/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 219w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_336,w_263/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 263w, 
+https://res.cloudinary.com/badboylu/image/upload/c_scale,h_671,w_526/v1596008969/c98132204fc270649a31ba21002c4460_ozfvzg.jpg 526w" 
 sizes="(max-width: 273px) 100vw, 273px" /> 
 </a>
 <div class="hover-img"> 
