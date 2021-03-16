207d06880531194ddd8cefe49e1caa46278aafbe
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-16 08:41:13 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -3,9 +3,8 @@ $servername="dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $dBUsername="fn5xp8yu9vmphif8";
 $dBPassword="j0oarzobzrwqy40b";
 $dBName="eebooc1cx2mejqru";
-   $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
-   $username = $_GET['username'];
-   $authtoken = $_GET['auth'];
+$conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+$authtoken = $_GET['auth'];
 ?>
 <!DOCTYPE html> <!--[if IE 8]><html class="ie ie8" lang="en-US"> <![endif]--> <!--[if !(IE 7) & !(IE 8)]><!--><html lang="en-US"> <!--<![endif]-->
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
@@ -87,8 +86,6 @@ href="cannacaps.html" />
 	<div class="container">
 	<div class="topbar-content">
 	<div class="top-bar-left"> 
-	<span> <?php echo $_GET['username']; ?></span><span> </span><span>[<a href="Logout.php?&auth=<?php echo $_GET['auth'];?>"><span style="color:red;"> Log out</span></a>]</span>
-
 	</div>
 	<div class="top-bar-right">
 	<div class="topbar-menu">
