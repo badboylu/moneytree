7c33371cea04cbb90e26a145a8a87cecf16de49b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-26 20:55:22 +0200

    Update Distro.php

--- Distro.php
+++ Distro.php
@@ -252,15 +252,15 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-3 col-md-6 xl-50">
-                        <div class="card o-hidden widget-cards">
-                            <div class="bg-primary card-body">
+                   <div class="col-xl-3 col-md-6 xl-50">
+                        <div class="card o-hidden  widget-cards">
+                            <div class="bg-secondary card-body">
                                 <div class="media static-top-widget row">
                                     <div class="icons-widgets col-4">
-                                        <div class="align-self-center text-center"><i data-feather="message-square" class="font-primary"></i></div>
+                                        <div class="align-self-center text-center"><i data-feather="box" class="font-secondary"></i></div>
                                     </div>
                                     <div class="media-body col-8"><span class="m-0">Prepared orders</span>
-                                        <h3 class="mb-0"><span class="counter">893</span><small> Today</small></h3>
+                                        <h3 class="mb-0"><span class="counter">9856</span><small> Today</small></h3>
                                     </div>
                                 </div>
                             </div>
@@ -294,15 +294,15 @@
                             </div>
                         </div>
                     </div>
-                      <div class="col-xl-3 col-md-6 xl-50">
-                        <div class="card o-hidden  widget-cards">
-                            <div class="bg-secondary card-body">
+                    <div class="col-xl-3 col-md-6 xl-50">
+                        <div class="card o-hidden widget-cards">
+                            <div class="bg-primary card-body">
                                 <div class="media static-top-widget row">
                                     <div class="icons-widgets col-4">
-                                        <div class="align-self-center text-center"><i data-feather="box" class="font-secondary"></i></div>
+                                        <div class="align-self-center text-center"><i data-feather="message-square" class="font-primary"></i></div>
                                     </div>
                                     <div class="media-body col-8"><span class="m-0">Cancelled deliveries</span>
-                                        <h3 class="mb-0"><span class="counter">9856</span><small> Today</small></h3>
+                                        <h3 class="mb-0"><span class="counter">893</span><small> Today</small></h3>
                                     </div>
                                 </div>
                             </div>
