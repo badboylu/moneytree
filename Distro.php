86a0709387fe1f5c27df570d1e1f17f61816fc36
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-26 20:46:43 +0200

    Update Distro.php

--- Distro.php
+++ Distro.php
@@ -259,7 +259,7 @@
                                     <div class="icons-widgets col-4">
                                         <div class="align-self-center text-center"><i data-feather="message-square" class="font-primary"></i></div>
                                     </div>
-                                    <div class="media-body col-8"><span class="m-0">Collected orders</span>
+                                    <div class="media-body col-8"><span class="m-0">Prepared orders</span>
                                         <h3 class="mb-0"><span class="counter">893</span><small> Today</small></h3>
                                     </div>
                                 </div>
@@ -273,8 +273,36 @@
                                     <div class="icons-widgets col-4">
                                         <div class="align-self-center text-center"><i data-feather="users" class="font-danger"></i></div>
                                     </div>
-                                    <div class="media-body col-8"><span class="m-0">New Vendors</span>
-                                        <h3 class="mb-0">$ <span class="counter">45631</span><small> This Month</small></h3>
+                                    <div class="media-body col-8"><span class="m-0">Collected orders</span>
+                                        <h3 class="mb-0"><span class="counter">45631</span><small> This Month</small></h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 xl-50">
+                        <div class="card o-hidden widget-cards">
+                            <div class="bg-warning card-body">
+                                <div class="media static-top-widget row">
+                                    <div class="icons-widgets col-4">
+                                        <div class="align-self-center text-center"><i data-feather="navigation" class="font-warning"></i></div>
+                                    </div>
+                                    <div class="media-body col-8"><span class="m-0">Delivered orders</span>
+                                        <h3 class="mb-0"><span class="counter">6659</span><small> Today</small></h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 xl-50">
+                        <div class="card o-hidden  widget-cards">
+                            <div class="bg-secondary card-body">
+                                <div class="media static-top-widget row">
+                                    <div class="icons-widgets col-4">
+                                        <div class="align-self-center text-center"><i data-feather="box" class="font-secondary"></i></div>
+                                    </div>
+                                    <div class="media-body col-8"><span class="m-0">Cancelled deliveries</span>
+                                        <h3 class="mb-0"><span class="counter">9856</span><small> Today</small></h3>
                                     </div>
                                 </div>
                             </div>
