281bc6c6930f6201887f2e59d05c5ff8c51ac568
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-27 19:08:47 +0200

    Update Distro.php

--- Distro.php
+++ Distro.php
@@ -115,6 +115,17 @@
                             </li>
                         </ul>
                     </li>
+                    <li><a class="sidebar-header" href="#"><i data-feather="chrome"></i><span>Stock management</span></a>
+                     <ul class="sidebar-submenu">
+                            <li><a href="#"><i class="fa fa-circle"></i>Recieve</a></li>
+                     </ul>  
+                    </li>
+                    <li><a class="sidebar-header" href="#"><i data-feather="clipboard"></i><span>Order log</span></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="#"><i class="fa fa-circle"></i>Daily</a></li>
+                             <li><a href="#"><i class="fa fa-circle"></i>Monthly</a></li>
+                        </ul>
+                    </li>
                     <li><a class="sidebar-header" href="login.html"><i data-feather="log-in"></i><span>Logout</span></a>
                     </li>
                 </ul>
