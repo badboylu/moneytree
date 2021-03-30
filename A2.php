2941c0d9205386ce752185a2b6f6e6afd74ac4cb
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-03-30 09:26:03 +0200

    Update A2.php

--- A2.php
+++ A2.php
@@ -179,7 +179,7 @@ well as the attained ability to safely dispose of excess cannabis cultivated.
 Our aim as the Cannabis Health and Awareness NPO is to prevent the people of South Africa from involuntary violating the law by educating the people and offering our key disposal services.
 </p>
 <p>
-The Cannabis for Private Purposes Bill of 2020 [<a href="#.php"><span>read</span></a>]
+The Cannabis for Private Purposes Bill of 2020 [<a href="#.php"><span>Download</span></a>]
 </p>
 </html>
 
