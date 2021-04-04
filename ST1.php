2a6a957638b78e3820d6a9e8d720f92a31033f3b
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-04-04 22:58:18 +0200

    Update ST1.php

--- ST1.php
+++ ST1.php
@@ -23,6 +23,13 @@ $sql='SELECT * FROM userauth2 WHERE token=?';
 ?>
 <html lang="en">
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Multikart admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Multikart admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+
     <link rel="icon" href="favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <title>Distribution Agent</title>
