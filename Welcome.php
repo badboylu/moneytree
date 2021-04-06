a3d1a6641a5317898e0df6efd0b4f137007b247a
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-04-06 20:14:45 +0200

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -28,7 +28,7 @@ $token= bin2hex(random_bytes(16));
                         <p style="color:green;" >Cannabis Disposal Agency</p>
                         <p style="color:grey;" ><small>Powered by the Cannabis Health and Awareness NPO [<a href="A2.php"><span>about us<span></a>]</small></p>
                         <p style="color:grey;" ><small>How we operate [<a href="A3.php"><span>info<span></a>]</small></p>
-                        <p style="color:grey;" ><small>Operations login[<a href="Signin.php"><span>click here<span></a>]</small></p>
+                        <p style="color:grey;" ><small>Operations login [<a href="Signin.php"><span>click here<span></a>]</small></p>
                         <p><small>To enter you must acknowledge the following:</small></p>
                         <p><small>I am over 18 years of age and I have read these <a href="A1.php">terms and conditions</a>.</small></p>
                         <div class="page-links">
