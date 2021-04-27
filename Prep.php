ba16f23c3400ddc58fd41c79b8d6ad2bce901db2
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-04-27 18:24:40 +0200

    Update Prep.php

--- Prep.php
+++ Prep.php
@@ -6,20 +6,7 @@
 $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $authtoken=$_GET['auth'];
 $user=$_GET['username'];
-   $sql='SELECT * FROM userauth2 WHERE token=?';
-   $stmt=mysqli_stmt_init($conn);
- if(!mysqli_stmt_prepare($stmt,$sql)){
-       header("Location:Signin.php?error=SQL1");
-       exit();
-}
-   mysqli_stmt_bind_param($stmt,"s",$authtoken);
-   mysqli_stmt_execute($stmt);
-   mysqli_stmt_store_result($stmt);
-   $count=mysqli_stmt_num_rows($stmt);
- if (!$count>0){
-    header("Location:Signin.php?error=notloggedin");
-    exit();
-}
+   
 
 $query="SELECT * FROM oders WHERE idOrderConfirmation='Pending'";
 $result=mysqli_query($conn,$query);
