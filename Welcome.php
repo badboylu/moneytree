c65f7587b0fc1e156b47569bfe6988ab70d8e3a0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-05-01 17:19:33 +0200

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -5,7 +5,7 @@ $token= bin2hex(random_bytes(16));
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Port</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="iofrm-style.css">
