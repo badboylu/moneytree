f1ad75bddb0a6aa407925432a5cb711c98fd57a3
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-05-29 11:03:35 +0200

    Update process4.php

--- process4.php
+++ process4.php
@@ -11,12 +11,6 @@ while($row=mysqli_fetch_array($result)){
  $token[]=$row['idOrders'];
 }
 $ordertoken=Max($token);
-$query2='SELECT * FROM deliverytoken WHERE idOrderID="'.$authtoken.'" ';
-$result2=mysqli_query($conn,$query2);
-while($row2=mysqli_fetch_array($result2)){
- $token2[]=$row2['id'];
-}
-$ordertoken2=Max($token2);
 $sql="UPDATE oders SET idOrderConfirmation='Pending' WHERE idOrderToken='".$authtoken."' AND idOrders='".$ordertoken."' ";
 mysqli_query($conn,$sql);
 ?>
