9c2ac4ab33f322061c80f8f6b4d623c845523b4e
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-14 20:40:28 +0200

    Update BatchA.php

--- BatchA.php
+++ BatchA.php
@@ -303,7 +303,7 @@ per/gram
 <span>
 Powered by the SAPS [<a href="A3.php?auth=<?php echo $_GET['auth'];?>">About us</a>]<br>Terms and conditions [<a href="A1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">Here</a>]
 </span>
-<br>Contact us <a href="C.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">[here]</a></div></div></div></div></footer></div>
+<br>Contact us <a href="C.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">[Here]</a></div></div></div></div></footer></div>
 <div class="basel-close-side">
 </div>
 <div class="basel-toolbar icons-design-line basel-toolbar-label-show">
