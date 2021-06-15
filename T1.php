2b39e7befe7fc1eca3c250cce04bc71492f049f7
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-15 16:50:37 +0200

    Update T1.php

--- T1.php
+++ T1.php
@@ -33,7 +33,7 @@ $pending = 'Pending';
    $pe = $_GET['ePE'];
    $contact = $_GET['eContact'];
    $date = $_GET['eDate'];
-
+   
    $query="SELECT * FROM oders WHERE idOrderToken='".$authtoken."' ";
    $result=mysqli_query($conn,$query);
    $count=mysqli_num_rows($result);
@@ -42,7 +42,7 @@ $pending = 'Pending';
    $check[]=$row['idOrderConfirmation'];
    }
    if($check='Pending'||$check='Collected'||$check='Prepared'){
-   $query="SELECT * FROM oders WHERE idOrderToken='".$authtoken."'";
+   $query="SELECT * FROM oders WHERE idOrderToken='".$authtoken."' AND idOrderConfirmation='".$pending."' OR idOrderConfirmation='".$prepared."' OR idOrderConfirmation='".$collected."' ";
    $result=mysqli_query($conn,$query);
    while($row=mysqli_fetch_array($result)){
    $ordernumbers[]=$row['idOrders'];
@@ -54,18 +54,21 @@ $pending = 'Pending';
    $ordertoken[]=$row['idOrderToken'];
    }
    $token=Min($ordertoken); 
+  
    $query="SELECT * FROM oders WHERE idOrders='".$order."' ";
    $result=mysqli_query($conn,$query);
    while($row=mysqli_fetch_array($result)){
    $custitoken[]=$row['idOrderCustiCode'];
    }
    $code=Min($custitoken); 
+  
    $query="SELECT * FROM deliverytoken WHERE idOrderID='".$token."' AND idCustomerToken='".$code."' ";
    $result=mysqli_query($conn,$query);
    while($row=mysqli_fetch_array($result)){
    $ordernm[]=$row['id'];
    }
    $ordernmb=Min($ordernm); 
+  
    $sql="SELECT * FROM oders WHERE idOrderConfirmation=? AND idOrderToken=? AND idOrders=? ";
    $stmt=mysqli_stmt_init($conn);
    if(!mysqli_stmt_prepare($stmt,$sql)){
@@ -104,6 +107,7 @@ $pending = 'Pending';
     $prepared1='0';
     $collected1='0';
     $pending1='0';
+   }
    }else{
     $prepared1='0';
     $collected1='0';
@@ -132,8 +136,9 @@ $sql='SELECT idDate FROM address WHERE idOrderCustiCode="'.$code.'"';
    mysqli_stmt_bind_result($stmt, $hash1);
    while (mysqli_stmt_fetch($stmt)) { 
     $dateplaced=$hash1;
-    $timeplacedReal= date('H:i', strtotime($timeplaced));
-    $timeChange=date('H:i',strtotime('+45 minutes',strtotime($timeplaced)));
+	   
+$timeplacedReal= date('H:i', strtotime($timeplaced));
+$timeChange=date('H:i',strtotime('+45 minutes',strtotime($timeplaced)));
 }
 ?>
 <html>
