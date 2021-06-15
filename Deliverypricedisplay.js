db060426e14d880224d803f2f4f395f5b807f2b4
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-15 09:59:17 +0200

    Update Deliverypricedisplay.js

--- Deliverypricedisplay.js
+++ Deliverypricedisplay.js
@@ -1,6 +1,6 @@
 setInterval (function cartPriceDisplay(){
 let cartCost = localStorage.getItem('totalAmountNumber');
 cartCost = parseInt(cartCost);
-document.querySelector('.totalpricedelivery').textContent = cartCost + 30;
+document.querySelector('.totalpricedelivery').textContent = cartCost + 15;
 },1000);
 cartPriceDisplay();
