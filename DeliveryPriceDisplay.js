28c914be0eaed9f6ab70688794d194f713de2217
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-15 12:18:10 +0200

    Update DeliveryPriceDisplay.js

--- DeliveryPriceDisplay.js
+++ DeliveryPriceDisplay.js
@@ -2,12 +2,12 @@ function deliveryPriceDisplay(){
 let check = localStorage.getItem('discount');
 if(check){
 let deliveryCost1 = localStorage.getItem('discount');
-let deliveryFee1 = 30;
+let deliveryFee1 = 15;
 deliveryCost2 = parseInt(deliveryCost1) + deliveryFee1;
 document.querySelector('.totalOrderDelivery').textContent = deliveryCost2;
 }else{
 let deliveryCost = localStorage.getItem('totalAmountNumber');
-let deliveryFee = 30;
+let deliveryFee = 15;
 deliveryCost = parseInt(deliveryCost) + deliveryFee;
 document.querySelector('.totalOrderDelivery').textContent = deliveryCost;
 }
