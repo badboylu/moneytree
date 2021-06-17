729dc8b863e3e5ad98097bbc2308bc9fa335ce4c
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-17 09:22:15 +0200

    Update BatchC.php

--- BatchC.php
+++ BatchC.php
@@ -311,7 +311,7 @@ per/gram
 </i> 
 2021 
 <span>
-Powered by the SAPS and TIA[<a href="A3.php?auth=<?php echo $_GET['auth'];?>">About us</a>]<br>Terms and conditions [<a href="A1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">Here</a>]
+Powered by the SAPS and TIA [<a href="A3.php?auth=<?php echo $_GET['auth'];?>">About us</a>]<br>Terms and conditions [<a href="A1.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">Here</a>]
 </span><br>
 Contact us <a href="C.php?username=<?php echo $_GET['username'];?>&auth=<?php echo $_GET['auth'];?>">[Here]</a></div></div></div></div></footer></div>
 <div class="basel-close-side">
