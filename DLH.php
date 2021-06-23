29fe5829416dd13d395ff71a6b6618c990f040a0
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-23 22:28:56 +0200

    Update DLH.php

--- DLH.php
+++ DLH.php
@@ -176,7 +176,7 @@ $count2=mysqli_num_rows($result2);
                             </div>
                         </div>
                         <div class="col-lg-6">
-                            
+                            <span style="color:grey;"><?php echo $date; ?></span>
                         </div>
                     </div>
                 </div>
