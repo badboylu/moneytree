acbb92f1cfd69cf938354335decb24a9e3908bc7
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-24 23:39:56 +0200

    Update Port.php

--- Port.php
+++ Port.php
@@ -9,6 +9,7 @@ $token= bin2hex(random_bytes(16));
     <meta name="description" content="Canibu is a website application that is powered by the South African police service (SAPS) and the Technology innovation agency (TIA) for the purpose of disposing cannabis.">
     <meta name="keywords" content="Canibu, canibu, canibu.co.za, cannabis disposal agency, cannabis, disposal, powered by SAPS, powered by TIA">
     <meta name="author" content="Kahrent Technology Africa (Pty) Ltd">
+    <meta name="google-site-verification" content="GBPu-caHlQbXXit81AZCLNw98tL0XUFABpwSJUSdiUI" />
     <title>Canibu - Port</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-all.min.css">
