31ffe1afd989eac410115783bd67c6e8a88af270
BadBoyLu <68569513+badboylu@users.noreply.github.com>
2021-06-27 12:16:48 +0200

    Update Items.php

--- Items.php
+++ Items.php
@@ -7,9 +7,8 @@ $conn=mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
 $authtoken = $_GET['auth'];
 ?>
 <html lang="en-US">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Canibu is a website application that is powered by the South African police service (SAPS) and the Technology innovation agency (TIA) for the purpose of disposing cannabis.">
